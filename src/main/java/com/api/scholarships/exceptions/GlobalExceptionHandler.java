7664758747a7795723af07b6f541b2003d505ccd
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-05 17:24:27 -0500

    Feat: Creation of the method to handle NotFound  exceptions

--- src/main/java/com/api/scholarships/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/api/scholarships/exceptions/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -14,6 +13,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   public final ProblemDetail handlerBadRequestException(BadRequestException badRequest){
     ProblemDetail body=ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,"BAD REQUEST");
     body.setDetail(badRequest.getMessage());
+    return body;
+  }
+
+  @ExceptionHandler(NotFoundException.class)
+  public final ProblemDetail handlerNotFoundException(NotFoundException notFoundException){
+    ProblemDetail body=ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,"NOT FOUND");
+    body.setDetail(notFoundException.getMessage());
     return body;
   }
 
