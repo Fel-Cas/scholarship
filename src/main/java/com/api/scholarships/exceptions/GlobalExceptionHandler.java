cb1852abec350f0e82d5080786d1b93bc7d7d9ba
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-05 17:27:01 -0500

    Feat: Creation of method to manage errors in DTOs

--- src/main/java/com/api/scholarships/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/api/scholarships/exceptions/GlobalExceptionHandler.java
@@ -2,9 +2,16 @@ package com.api.scholarships.exceptions;
 
 
 import org.springframework.http.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -21,6 +28,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     ProblemDetail body=ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,"NOT FOUND");
     body.setDetail(notFoundException.getMessage());
     return body;
+  }
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    Map<String,String> errors= new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach(error->{
+      String fieldName= ((FieldError) error).getField();
+      String message=error.getDefaultMessage();
+      errors.put(fieldName,message);
+    });
+    return new ResponseEntity<Object>(errors,HttpStatus.BAD_REQUEST);
   }
 
 }
