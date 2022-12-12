d24d1a5b6cdea669a2d72b34ae97641a39e0b3fb
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 00:02:31 -0500

    Feat: Adding error messages for LegalRepresentativeDTO class

--- src/main/java/com/api/scholarships/constants/Messages.java
+++ src/main/java/com/api/scholarships/constants/Messages.java
@@ -1,5 +1,16 @@
 package com.api.scholarships.constants;
 
 public class Messages {
-
+  //legalRepresentative
+  public static final String MESSAGE_NAME_REQUIRED = "Name is required";
+  public static final String MESSAGE_NAME_SIZE = "Name must be between 2 and 50 characters";
+  public static final String MESSAGE_SURNAME_REQUIRED = "Surname is required";
+  public static final String MESSAGE_SURNAME_SIZE = "Surname must be between 2 and 50 characters";
+  public static final String MESSAGE_EMAIL_REQUIRED = "Email is required";
+  public static final String MESSAGE_EMAIL_SIZE = "Email must be between 11 and 50 characters";
+  public static final String MESSAGE_EMAIL_VALID = "Email must be valid";
+  public static final String MESSAGE_DNI_REQUIRED = "DNI is required";
+  public static final String MESSAGE_DNI_SIZE = "DNI must be between 10 and 15 characters";
+  public static final String MESSAGE_PASSWORD_REQUIRED = "Password is required";
+  public static final String MESSAGE_PASSWORD_SIZE = "Password must be between 8 and 50 characters";
 }
