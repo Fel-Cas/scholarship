deb34f7f00b1e8bb0abbd66fd800acb31d90377e
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 16:40:40 -0500

    Feat: Adding method to find a legal representative by his dni

--- src/main/java/com/api/scholarships/repositories/LegalRepresentativeRepository.java
+++ src/main/java/com/api/scholarships/repositories/LegalRepresentativeRepository.java
@@ -4,10 +4,12 @@ import com.api.scholarships.entities.LegalRepresentative;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LegalRepresentativeRepository extends JpaRepository<LegalRepresentative, Long> {
   boolean existsByEmail(String email);
   boolean existsByDni(String dni);
-
+  Optional<LegalRepresentative> findByDni(String dni);
 
 }
