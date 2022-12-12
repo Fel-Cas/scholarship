72f2cd7146955674691cf7f5d8de97f13934f681
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 16:11:50 -0500

    Feat: Adding support for URIs when trying to search for something by id

--- src/main/java/com/api/scholarships/constants/Endpoints.java
+++ src/main/java/com/api/scholarships/constants/Endpoints.java
@@ -1,5 +1,9 @@
 package com.api.scholarships.constants;
 
 public class Endpoints {
+  // Id
+  public static final String ID = "/{id}";
+
+  //LegalRepresentative
   public static final String LEGAL_REPRESENTATIVE = "/api/v1/scholarships/legal-representative";
 }
