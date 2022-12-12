0733730feb18e89b5cb27e9a634ead28009e8f6b
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 00:19:33 -0500

    Feat: Creation of the LegalRepresentativeDTOResponse class to display information to users

---
+++ src/main/java/com/api/scholarships/dtos/LegalRepresentativeDTOResponse.java
@@ -0,0 +1,40 @@
+package com.api.scholarships.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Data
+public class LegalRepresentativeDTOResponse {
+    @JsonProperty("id")
+    Long id;
+    @JsonProperty("name")
+    String name;
+    @JsonProperty("surname")
+    String surname;
+    @JsonProperty("email")
+    String email;
+    @JsonProperty("dni")
+    String dni;
+    @JsonProperty("createdAt")
+    String createdAt;
+    @JsonProperty("updatedAt")
+    String updatedAt;
+
+    @Override
+    public String toString() {
+        return "LegalRepresentativeDTOResponse{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", surname='" + surname + '\'' +
+            ", email='" + email + '\'' +
+            ", dni='" + dni + '\'' +
+            ", createdAt='" + createdAt + '\'' +
+            ", updatedAt='" + updatedAt + '\'' +
+            '}';
+    }
+}
