e02304a95b146400d3a46a0748f2f5b68db2b91a
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 00:07:00 -0500

    Feat: Creation of the LegalRepresentative service interface with all the methods to be implemented

---
+++ src/main/java/com/api/scholarships/services/interfaces/LegalRepresentativeService.java
@@ -0,0 +1,14 @@
+package com.api.scholarships.services.interfaces;
+
+import com.api.scholarships.dtos.LegalRepresentativeDTO;
+import com.api.scholarships.dtos.LegalRepresentativeResponse;
+import com.api.scholarships.entities.LegalRepresentative;
+
+public interface LegalRepresentativeService {
+  public LegalRepresentative save(LegalRepresentativeDTO legalRepresentative);
+  public LegalRepresentativeResponse getAllLegalRepresentatives(int page, int size);
+  public LegalRepresentativeResponse getLegalRepresentativeById(Long id);
+  public LegalRepresentative getLegalRepresentativeByEmail(String email);
+  public LegalRepresentative updateLegalRepresentative(Long id, LegalRepresentativeDTO legalRepresentative);
+  public void deleteLegalRepresentative(Long id);
+}
