4a037c82f0fa21b01a15c07ac374beb3fa377b21
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 15:08:58 -0500

    Feat: Creation of the functionality to create a new legal representative

--- src/main/java/com/api/scholarships/controllers/LegalRepresentativeController.java
+++ src/main/java/com/api/scholarships/controllers/LegalRepresentativeController.java
@@ -5,6 +5,7 @@ import com.api.scholarships.dtos.LegalRepresentativeDTO;
 import com.api.scholarships.dtos.LegalRepresentativeDTOResponse;
 import com.api.scholarships.entities.LegalRepresentative;
 import com.api.scholarships.mappers.LegalRepresentativeMapper;
+import com.api.scholarships.services.interfaces.LegalRepresentativeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(Endpoints.LEGAL_REPRESENTATIVE)
 public class LegalRepresentativeController {
 
+  @Autowired
+  private LegalRepresentativeMapper legalRepresentativeMapper;
+  @Autowired
+  private LegalRepresentativeService legalRepresentativeService;
+
+  @PostMapping()
+  public ResponseEntity<LegalRepresentativeDTOResponse> save(@Valid @RequestBody LegalRepresentativeDTO legalRepresentativeDTO){
+    LegalRepresentative legalRepresentative = legalRepresentativeService.save(legalRepresentativeDTO);
+    return ResponseEntity.ok(legalRepresentativeMapper.legalRepresentativeToLegalRepresentativeDTOResponse(legalRepresentative));
+  }
 }
--- src/main/java/com/api/scholarships/services/implementation/LegalRepresentativeServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/LegalRepresentativeServiceImp.java
@@ -1,17 +1,29 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.LegalRepresentativeDTO;
 import com.api.scholarships.dtos.LegalRepresentativeResponse;
 import com.api.scholarships.entities.LegalRepresentative;
+import com.api.scholarships.exceptions.BadRequestException;
+import com.api.scholarships.mappers.LegalRepresentativeMapper;
+import com.api.scholarships.repositories.LegalRepresentativeRepository;
 import com.api.scholarships.services.interfaces.LegalRepresentativeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LegalRepresentativeServiceImp implements LegalRepresentativeService {
 
+  @Autowired
+  private LegalRepresentativeRepository legalRepresentativeRepository;
+  @Autowired
+  private LegalRepresentativeMapper legalRepresentativeMapper;
+
   @Override
   public LegalRepresentative save(LegalRepresentativeDTO legalRepresentative) {
-    return null;
+    if(legalRepresentativeRepository.existsByEmail(legalRepresentative.getEmail())) throw new BadRequestException(Messages.MESSAGE_LEGAL_REPRESENTATIVE_BAD_REQUEST_CREATE_WITH_WRONG_EMAIL);
+    if(legalRepresentativeRepository.existsByDni(legalRepresentative.getDni())) throw new BadRequestException(Messages.MESSAGE_LEGAL_REPRESENTATIVE_BAD_REQUEST_CREATE_WITH_WRONG_DNI);
+    return legalRepresentativeRepository.save(legalRepresentativeMapper.legalRepresentativeDTOToLegalRepresentative(legalRepresentative));
   }
 
   @Override
