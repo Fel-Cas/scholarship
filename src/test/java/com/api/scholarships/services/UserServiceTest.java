95c13edc29cff69603ee9271ba02c463660e0051
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-13 18:07:19 -0500

    Feat: Test UserService, test to save a user

--- src/test/java/com/api/scholarships/services/UserServiceTest.java
+++ src/test/java/com/api/scholarships/services/UserServiceTest.java
@@ -1,10 +1,13 @@
 package com.api.scholarships.services;
 
+import com.api.scholarships.dtos.UserDTO;
 import com.api.scholarships.entities.User;
 import com.api.scholarships.mappers.UserMapper;
 import com.api.scholarships.repositories.UserRepository;
 import com.api.scholarships.services.implementation.UserServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,6 +17,10 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 class UserServiceTest {
@@ -25,6 +32,7 @@ class UserServiceTest {
   @InjectMocks
   private UserServiceImp userService;
   private User user;
+  private UserDTO userDTO;
 
   @BeforeEach
   void init() {
@@ -38,5 +46,35 @@ class UserServiceTest {
         .updatedAt(Instant.now())
         .createdAt(Instant.now())
         .build();
+    userDTO = UserDTO.builder()
+        .name("Andrés Felipe")
+        .surname("Castro Monsalve")
+        .dni("1001233147")
+        .email("andres.cmonsalve@gmail.com")
+        .password("123456")
+        .build();
+  }
+
+  @Test
+  @DisplayName("Test UserService,Test to save a new user")
+  void testSaveUser() {
+    //given
+    given(userRepository.existsByEmail(anyString())).willReturn(false);
+    given(userRepository.existsByDni(anyString())).willReturn(false);
+    given(userRepository.save(any(User.class))).willReturn(user);
+    given(userMapper.userDTOToUser(any(UserDTO.class))).willReturn(user);
+    //when
+    User userSaved = userService.save(userDTO);
+    //then
+    assertAll(
+        () -> assertNotNull(userSaved),
+        () -> assertEquals(userSaved.getName(), userDTO.getName()),
+        () -> assertEquals(userSaved.getSurname(), userDTO.getSurname()),
+        () -> assertEquals(userSaved.getDni(), userDTO.getDni()),
+        () -> assertEquals(userSaved.getEmail(), userDTO.getEmail()),
+        () -> assertEquals(userSaved.getPassword(), userDTO.getPassword()),
+        ()-> assertNotNull(userSaved.getCreatedAt()),
+        ()-> assertNotNull(userSaved.getUpdatedAt())
+    );
   }
 }
