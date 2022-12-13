5ed9b3ad30c6070393d1f91fd7a82c6873790d90
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-12 20:29:53 -0500

    Feat: LegalRepresentativeRepository Test to create a new legal representative

--- src/test/java/com/api/scholarships/repositories/LegalRepresentativeRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/LegalRepresentativeRepositoryTest.java
@@ -2,14 +2,17 @@ package com.api.scholarships.repositories;
 
 import com.api.scholarships.entities.LegalRepresentative;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@ActiveProfiles("test")
 class LegalRepresentativeRepositoryTest {
 
   @Autowired
@@ -23,6 +26,22 @@ class LegalRepresentativeRepositoryTest {
       .surname("Castro Monsalve")
       .dni("1001233147")
       .email("andres.c@gmail.com")
+      .password("123456")
+      .createdAt(LocalDateTime.now())
+      .updatedAt(LocalDateTime.now())
       .build();
+  }
+
+  @Test
+  @DisplayName("Test LegalRepresentativeRepository,Test save legal representative")
+  void testSaveLegalRepresentative(){
+    //given
+    //when
+    LegalRepresentative legalRepresentativeSaved = legalRepresentativeRepository.save(legalRepresentative);
+    //then
+    assertNotNull(legalRepresentativeSaved);
+    assertThat(legalRepresentativeSaved.getId()).isGreaterThan(0);
+    assertNotNull(legalRepresentativeSaved.getCreatedAt());
+    assertNotNull(legalRepresentativeSaved.getUpdatedAt());
   }
 }
