01f8e81542e681766da421c7b5d7b4f8c28527bd
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 16:30:49 -0500

    Feat: Add message to inform users when they  try to find a role by name that doesn't exist

--- src/main/java/com/api/scholarships/constants/Messages.java
+++ src/main/java/com/api/scholarships/constants/Messages.java
@@ -14,9 +14,13 @@ public class Messages {
   public static final String MESSAGE_PASSWORD_REQUIRED = "Password is required";
   public static final String MESSAGE_PASSWORD_SIZE = "Password must be between 8 and 50 characters";
 
-  //Legal Representative Service
+  //User Service
   public static final String MESSAGE_USER_BAD_REQUEST_CREATE_WITH_WRONG_DNI ="Already exists a legal representative with this DNI";
   public static final String MESSAGE_USER_BAD_REQUEST_CREATE_WITH_WRONG_EMAIL ="Already exists a legal representative with this email";
   public static final String MESSAGE_USER_NOT_FOUND ="Not exists a legal representative with this id %s";
   public static final String MESSAGE_USER_NOT_FOUND_BY_DNI ="Not exists a legal representative with this dni %s";
+
+  //Role Service
+  public static final String MESSAGE_ROLE_NOT_FOUND ="Not exists a role with this id %s";
+
 }
