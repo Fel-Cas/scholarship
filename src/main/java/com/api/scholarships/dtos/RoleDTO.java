5adf599ab894a1c64f154c693a7a5942925b53e3
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 15:40:06 -0500

    Feat: Creation of the RoleDTO class to display information  to users

---
+++ src/main/java/com/api/scholarships/dtos/RoleDTO.java
@@ -0,0 +1,15 @@
+package com.api.scholarships.dtos;
+
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
+@Builder
+public class RoleDTO {
+  private Long id;
+  private String nameRole;
+}
