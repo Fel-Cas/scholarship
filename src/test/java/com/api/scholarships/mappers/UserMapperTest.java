91e4940621c809e685ca461b365b4a788ff7088b
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-13 21:25:27 -0500

    Feat: Creation of the UserMapperTest class for testinf UserMapper methods

---
+++ src/test/java/com/api/scholarships/mappers/UserMapperTest.java
@@ -0,0 +1,12 @@
+package com.api.scholarships.mappers;
+
+
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperTest {
+
+  private UserMapper userMapper= Mappers.getMapper(UserMapper.class);
+
+}
