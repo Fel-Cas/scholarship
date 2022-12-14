9bc078fcbb012d0ab5cc95f23d69495449dbe747
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 15:38:58 -0500

    Feat: Creation of the role repository

---
+++ src/main/java/com/api/scholarships/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.api.scholarships.repositories;
+
+import com.api.scholarships.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
