91692ff9f98cf94dd0b1b6722b79b885c8598b8a
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-13 21:26:15 -0500

    Feat: Test userToUserDTOResponse

--- src/test/java/com/api/scholarships/mappers/UserMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/UserMapperTest.java
@@ -1,12 +1,47 @@
 package com.api.scholarships.mappers;
 
-
+import com.api.scholarships.dtos.UserDTOResponse;
+import com.api.scholarships.entities.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserMapperTest {
 
   private UserMapper userMapper= Mappers.getMapper(UserMapper.class);
+
+  @Test
+  @DisplayName("Test userToUserDTOResponse")
+  void userToUserDTOResponse() {
+    //given
+    User user = User.builder()
+        .id(1L)
+        .name("Andrés Felipe")
+        .surname("Castro Monsalve")
+        .dni("12345678912")
+        .email("andres.cmonsalve@gmail.com")
+        .password("12345678")
+        .updatedAt(Instant.parse("2021-05-01T00:00:00Z"))
+        .createdAt(Instant.parse("2021-05-01T00:00:00Z"))
+        .build();
+    //when
+    UserDTOResponse userDTOResponse = userMapper.userToUserDTOResponse(user);
+    //then
+    assertAll(
+        () -> assertEquals(user.getId(), userDTOResponse.getId()),
+        () -> assertEquals(user.getName(), userDTOResponse.getName()),
+        () -> assertEquals(user.getSurname(), userDTOResponse.getSurname()),
+        () -> assertEquals(user.getDni(), userDTOResponse.getDni()),
+        () -> assertEquals(user.getEmail(), userDTOResponse.getEmail()),
+        () -> assertEquals(user.getUpdatedAt().toString(), userDTOResponse.getUpdatedAt()),
+        () -> assertEquals(user.getCreatedAt().toString(), userDTOResponse.getCreatedAt())
+    );
+
+  }
 
 }
