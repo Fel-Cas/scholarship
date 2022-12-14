d34f7165933700471ad66a25185b4db1686f2172
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-13 21:31:37 -0500

    Feat: Test list of users to list of userDTOResponse

--- src/test/java/com/api/scholarships/mappers/UserMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/UserMapperTest.java
@@ -9,6 +9,7 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.Instant;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -65,6 +66,37 @@ class UserMapperTest {
         () -> assertEquals(userDTO.getDni(), user.getDni()),
         () -> assertEquals(userDTO.getEmail(), user.getEmail()),
         () -> assertEquals(userDTO.getPassword(), user.getPassword())
+    );
+  }
+
+  @Test
+  @DisplayName("Test list of users to list of userDTOResponse")
+  void userToUserDTOResponseList() {
+    //given
+    User user = User.builder()
+        .id(1L)
+        .name("Andrés Felipe")
+        .surname("Castro Monsalve")
+        .dni("12345678912")
+        .email("andres.cmonsalve@gmail.com")
+        .password("12345678")
+        .updatedAt(Instant.parse("2021-05-01T00:00:00Z"))
+        .createdAt(Instant.parse("2021-05-01T00:00:00Z"))
+        .build();
+
+    List<User> users = List.of(user);
+    //when
+    List<UserDTOResponse> usersDTOResponse = userMapper.userToUserDTOResponse(users);
+    //then
+    assertAll(
+        ()->assertEquals(usersDTOResponse.size(),users.size()),
+        () -> assertEquals(users.get(0).getId(), usersDTOResponse.get(0).getId()),
+        () -> assertEquals(users.get(0).getName(), usersDTOResponse.get(0).getName()),
+        () -> assertEquals(users.get(0).getSurname(), usersDTOResponse.get(0).getSurname()),
+        () -> assertEquals(users.get(0).getDni(), usersDTOResponse.get(0).getDni()),
+        () -> assertEquals(users.get(0).getEmail(), usersDTOResponse.get(0).getEmail()),
+        () -> assertEquals(users.get(0).getUpdatedAt().toString(), usersDTOResponse.get(0).getUpdatedAt()),
+        () -> assertEquals(users.get(0).getCreatedAt().toString(), usersDTOResponse.get(0).getCreatedAt())
     );
   }
 }
