d25e77596011f99293c536dd1499abd158d7466e
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 16:32:07 -0500

    Feat: Functionality to find role by name

--- src/main/java/com/api/scholarships/services/implementation/RoleServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/RoleServiceImp.java
@@ -1,15 +1,27 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.RoleResponse;
 import com.api.scholarships.entities.Role;
+import com.api.scholarships.exceptions.NotFoundException;
+import com.api.scholarships.repositories.RoleRepository;
 import com.api.scholarships.services.interfaces.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RoleServiceImp implements RoleService {
+    @Autowired
+    private RoleRepository roleRepository;
     @Override
     public Role findByName(String name) {
-        return null;
+        Optional<Role> roleFound = roleRepository.findByName(name.toUpperCase());
+        if(roleFound.isEmpty()){
+            throw new NotFoundException(Messages.MESSAGE_ROLE_NOT_FOUND.formatted(name.toUpperCase()));
+        }
+        return roleFound.get();
     }
 
     @Override
