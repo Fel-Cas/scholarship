a9e07073a8d2bae1ce252449b6e3407b51888553
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 16:53:37 -0500

    Feat: Functionality to find all roles

--- src/main/java/com/api/scholarships/controllers/RoleController.java
+++ src/main/java/com/api/scholarships/controllers/RoleController.java
@@ -1,15 +1,14 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.constants.Endpoints;
+import com.api.scholarships.constants.PaginationRequest;
 import com.api.scholarships.dtos.RoleDTO;
+import com.api.scholarships.dtos.RoleResponse;
 import com.api.scholarships.mappers.RoleMapper;
 import com.api.scholarships.services.interfaces.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(Endpoints.ROLES)
@@ -22,5 +21,15 @@ public class RoleController {
   @GetMapping(Endpoints.ID)
   public ResponseEntity<RoleDTO> getRoleById(@PathVariable Long id){
     return ResponseEntity.ok(roleMapper.roleToRoleDTO(roleService.findById(id)));
+  }
+
+  @GetMapping()
+  public ResponseEntity<RoleResponse> getRoles(
+      @RequestParam(name ="numberPage" ,defaultValue = PaginationRequest.DEFAULT_NUMBER_PAGE,required = false) int page,
+      @RequestParam(name = "pageSize",defaultValue = PaginationRequest.DEFAULT_PAGE_SIZE,required = false) int size,
+      @RequestParam(name = "sort",defaultValue = PaginationRequest.DEFAULT_SORT_BY,required = false) String sort,
+      @RequestParam(name = "order",defaultValue = PaginationRequest.DEFAULT_SORT_DIR, required = false) String order
+  ){
+    return ResponseEntity.ok(roleService.findAll(page,size,sort,order));
   }
 }
--- src/main/java/com/api/scholarships/services/implementation/RoleServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/RoleServiceImp.java
@@ -4,9 +4,14 @@ import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.RoleResponse;
 import com.api.scholarships.entities.Role;
 import com.api.scholarships.exceptions.NotFoundException;
+import com.api.scholarships.mappers.RoleMapper;
 import com.api.scholarships.repositories.RoleRepository;
 import com.api.scholarships.services.interfaces.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +20,8 @@ import java.util.Optional;
 public class RoleServiceImp implements RoleService {
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private RoleMapper roleMapper;
     @Override
     public Role findByName(String name) {
         Optional<Role> roleFound = roleRepository.findByNameRole(name.toUpperCase());
@@ -35,7 +42,18 @@ public class RoleServiceImp implements RoleService {
 
     @Override
     public RoleResponse findAll(int page, int size, String sort, String order) {
-        return null;
+        Sort sortDirection=order.equalsIgnoreCase(Sort.Direction.ASC.name())?Sort.by(sort).ascending():Sort.by(sort).descending();
+        Pageable pageable= PageRequest.of(page,size,sortDirection);
+        Page<Role> rolesFond=roleRepository.findAll(pageable);
+        return RoleResponse
+                .builder()
+                .content(roleMapper.roleToRoleDTO(rolesFond.getContent()))
+                .numberPage(rolesFond.getNumber())
+                .sizePage(rolesFond.getSize())
+                .totalElements(rolesFond.getTotalElements())
+                .totalPages(rolesFond.getTotalPages())
+                .lastOne(rolesFond.isLast())
+                .build();
     }
 }
 
