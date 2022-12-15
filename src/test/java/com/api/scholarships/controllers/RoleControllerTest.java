16f9f145a8917302d6d4261f12811dc7387391e9
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 22:25:52 -0500

    Feat: Creation of the RoleControllerTest class where the tests role controller will be located

---
+++ src/test/java/com/api/scholarships/controllers/RoleControllerTest.java
@@ -0,0 +1,9 @@
+package com.api.scholarships.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebMvcTest(RoleController.class)
+class RoleControllerTest {
+
+}
