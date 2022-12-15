9447deab4f63b237f8dd6e845eb625e343c3367e
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 20:27:59 -0500

    Refactor: userDTOToUser method deleted

--- src/main/java/com/api/scholarships/mappers/UserMapper.java
+++ src/main/java/com/api/scholarships/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package com.api.scholarships.mappers;
 
 import com.api.scholarships.dtos.UserDTO;
 import com.api.scholarships.dtos.UserDTOResponse;
+import com.api.scholarships.entities.Role;
 import com.api.scholarships.entities.User;
 import org.mapstruct.Mapper;
 
@@ -11,5 +12,4 @@ import java.util.List;
 public interface UserMapper {
   UserDTOResponse userToUserDTOResponse(User user);
   List<UserDTOResponse> userToUserDTOResponse(Iterable<User> legalRepresentative);
-  User userDTOToUser(UserDTO userDTO);
 }
