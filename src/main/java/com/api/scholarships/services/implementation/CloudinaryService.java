0292a4f7b655bab2e77f539163bbb74082605aae
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-15 13:16:06 -0500

    Feat: Configuration to make the cloudDinary connection

--- src/main/java/com/api/scholarships/services/implementation/CloudinaryService.java
+++ src/main/java/com/api/scholarships/services/implementation/CloudinaryService.java
@@ -1,12 +1,24 @@
 package com.api.scholarships.services.implementation;
 
 import com.api.scholarships.services.interfaces.CloudService;
+import com.cloudinary.Cloudinary;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CloudinaryService implements CloudService {
+    private final Cloudinary cloudinary;
+    private final Dotenv dotenv=Dotenv.load();
+    private CloudinaryService(){
+        Map<String,String> valuesCloudinary=new HashMap<>();
+        valuesCloudinary.put("cloud_name",dotenv.get("CLOUD_NAME"));
+        valuesCloudinary.put("api_key",dotenv.get("API_KEY"));
+        valuesCloudinary.put("api_sercret",dotenv.get("API_SECRET"));
+        cloudinary = new Cloudinary(valuesCloudinary);
+    }
 
     @Override
     public Map upload(MultipartFile multipartFile) {
