e2115495eab58873b71cc55450ee279167fbff48
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 22:44:57 -0500

    Feat: Test RoleController, test to find all roles

--- src/test/java/com/api/scholarships/controllers/RoleControllerTest.java
+++ src/test/java/com/api/scholarships/controllers/RoleControllerTest.java
@@ -1,6 +1,7 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.dtos.RoleDTO;
+import com.api.scholarships.dtos.RoleResponse;
 import com.api.scholarships.entities.Role;
 import com.api.scholarships.mappers.RoleMapper;
 import com.api.scholarships.services.interfaces.RoleService;
@@ -14,11 +15,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.mockito.ArgumentMatchers.anyLong;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -61,6 +64,34 @@ class RoleControllerTest {
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.id").value(1L))
         .andExpect(jsonPath("$.nameRole").value("ROLE_USER"));
+  }
+
+  @Test
+  @DisplayName("Test RoleController, test to find all roles")
+  void testFindAllRoles() throws Exception {
+    // given
+    RoleResponse roleResponse= RoleResponse.builder()
+        .content(List.of(roleDTO))
+        .numberPage(0)
+        .sizePage(10)
+        .lastOne(true)
+        .totalPages(1)
+        .totalElements(1L)
+        .build();
+
+    given(roleService.findAll(anyInt(), anyInt(), anyString(), anyString())).willReturn(roleResponse);
+    // when
+    ResultActions resultActions = mockMvc.perform(get(url));
+    // then
+    resultActions
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andExpect(jsonPath("$.content.size()").value(roleResponse.getContent().size()))
+        .andExpect(jsonPath("$.numberPage").value(roleResponse.getNumberPage()))
+        .andExpect(jsonPath("$.totalPages").value(roleResponse.getTotalPages()))
+        .andExpect(jsonPath("$.totalElements").value(roleResponse.getTotalElements()))
+        .andExpect(jsonPath("$.sizePage").value(roleResponse.getSizePage()))
+        .andExpect(jsonPath("$.lastOne").value(roleResponse.isLastOne()));
   }
 
 }
