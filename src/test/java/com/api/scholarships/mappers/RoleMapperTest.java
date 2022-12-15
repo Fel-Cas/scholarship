7c52a508329b651b6e55c8585161cd2178f814c6
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 22:49:28 -0500

    Feat: Test RoleMapper,Test roleToRoleDTO

--- src/test/java/com/api/scholarships/mappers/RoleMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/RoleMapperTest.java
@@ -1,5 +1,9 @@
 package com.api.scholarships.mappers;
 
+import com.api.scholarships.dtos.RoleDTO;
+import com.api.scholarships.entities.Role;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoleMapperTest {
 
   private RoleMapper roleMapper = Mappers.getMapper(RoleMapper.class);
+
+  @Test
+  @DisplayName("Test RoleMapper,Test roleToRoleDTO")
+  void roleToRoleDTO() {
+    //given
+    Role role= Role.builder()
+        .id(1L)
+        .nameRole("ROLE_USER")
+        .build();
+    //when
+    RoleDTO roleDTO = roleMapper.roleToRoleDTO(role);
+    //then
+    assertAll(
+        () -> assertEquals(role.getId(), roleDTO.getId()),
+        () -> assertEquals(role.getNameRole(), roleDTO.getNameRole())
+    );
+  }
 
 }
