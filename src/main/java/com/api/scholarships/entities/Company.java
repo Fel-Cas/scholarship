6e7f78172649fddce7ef5f5f0045dabe3bd2f85d
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-15 11:36:11 -0500

    Feat: Creation of the Company model

---
+++ src/main/java/com/api/scholarships/entities/Company.java
@@ -0,0 +1,26 @@
+package com.api.scholarships.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "companies")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+@Data
+public class Company {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name = "full_name")
+  private String name;
+  @Column(name = "address")
+  private String address;
+  @Column(name = "phone")
+  private String phone;
+  @Column(name = "email")
+  private String email;
+}
