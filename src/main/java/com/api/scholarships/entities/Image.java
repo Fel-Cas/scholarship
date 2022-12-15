b1827c6b7fe02e0170b52712ce9303ae6f0f62a6
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-15 11:35:45 -0500

    Feat: Creation of the photo model

---
+++ src/main/java/com/api/scholarships/entities/Image.java
@@ -0,0 +1,34 @@
+package com.api.scholarships.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "images")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Data
+public class Image {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(name = "image_url")
+  private String url;
+  @Column(name = "image_name")
+  private String name;
+  @Column(name = "image_id")
+  private String imageId;
+
+  @Override
+  public String toString() {
+    return "Image{" +
+        "id=" + id +
+        ", url='" + url + '\'' +
+        ", name='" + name + '\'' +
+        ", imageId='" + imageId + '\'' +
+        '}';
+  }
+}
