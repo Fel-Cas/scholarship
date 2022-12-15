d43d72fac5cd4f1887955b93ba50201deb22b8dd
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 21:33:50 -0500

    Feat: Test RoleRepository,Test save a new role

--- src/test/java/com/api/scholarships/repositories/RoleRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/RoleRepositoryTest.java
@@ -1,11 +1,39 @@
 package com.api.scholarships.repositories;
 
+import com.api.scholarships.entities.Role;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 @DataJpaTest
 @ActiveProfiles("test")
 class RoleRepositoryTest {
+  @Autowired
+  private RoleRepository roleRepository;
+  private Role role;
 
+  @BeforeEach
+  void init(){
+    role = Role.builder()
+        .id(1L)
+        .nameRole("ROLE_USER")
+        .build();
+  }
+
+  @Test
+  @DisplayName("Test RoleRepository,Test save a new role")
+  void testSaveRole(){
+    //given
+    //when
+    Role roleSaved = roleRepository.save(role);
+    //then
+    assertNotNull(roleSaved);
+    assertThat(roleSaved.getId()).isGreaterThan(0);
+    assertEquals(roleSaved.getNameRole(),role.getNameRole());
+  }
 }
