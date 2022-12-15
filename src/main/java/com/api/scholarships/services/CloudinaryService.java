c4c484ce008134711f8b8a4cbb603965d3a3386e
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-15 13:04:26 -0500

    Feat: Creation of the CloudinaryService class where the  methods to use cloud services will be implemented

---
+++ src/main/java/com/api/scholarships/services/CloudinaryService.java
@@ -0,0 +1,26 @@
+package com.api.scholarships.services;
+
+import com.api.scholarships.services.interfaces.CloudService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Map;
+
+public class CloudinaryService implements CloudService {
+
+    @Override
+    public Map upload(MultipartFile multipartFile) {
+        return null;
+    }
+
+    @Override
+    public Map delete(String id) {
+        return null;
+    }
+
+    @Override
+    public File convert(MultipartFile multipartFile) {
+        return null;
+    }
+}
+
