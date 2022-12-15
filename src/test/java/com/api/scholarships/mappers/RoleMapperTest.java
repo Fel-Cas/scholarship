110ef96d83b7db5608b84b8193ec1f1397a82c95
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 22:47:08 -0500

    Feat: Creation of the RoleMapperTest class where the tests role mapper will be located

---
+++ src/test/java/com/api/scholarships/mappers/RoleMapperTest.java
@@ -0,0 +1,11 @@
+package com.api.scholarships.mappers;
+
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleMapperTest {
+
+  private RoleMapper roleMapper = Mappers.getMapper(RoleMapper.class);
+
+}
