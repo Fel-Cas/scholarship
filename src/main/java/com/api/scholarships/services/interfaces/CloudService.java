292306b8ffacbfcfb1566d2c8286c06ff73c1ef4
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-15 13:20:37 -0500

    Feat: Add support for IOException to the  upload method

--- src/main/java/com/api/scholarships/services/interfaces/CloudService.java
+++ src/main/java/com/api/scholarships/services/interfaces/CloudService.java
@@ -3,11 +3,12 @@ package com.api.scholarships.services.interfaces;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public interface CloudService {
 
-  public Map upload(MultipartFile multipartFile);
+  public Map upload(MultipartFile multipartFile) throws IOException;
   public Map delete(String id);
   public File convert(MultipartFile multipartFile);
 }
