188280f04dffd892d871d9bd5e0c1ab2e1b1c034
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-14 21:36:06 -0500

    Feat: Test RoleRepository,Test find all roles

--- src/test/java/com/api/scholarships/repositories/RoleRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/RoleRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 @DataJpaTest
@@ -35,5 +37,16 @@ class RoleRepositoryTest {
     assertNotNull(roleSaved);
     assertThat(roleSaved.getId()).isGreaterThan(0);
     assertEquals(roleSaved.getNameRole(),role.getNameRole());
+  }
+  @Test
+  @DisplayName("Test RoleRepository,Test find all roles")
+  void testFindAllRoles(){
+    //given
+    roleRepository.save(role);
+    //when
+    List<Role>rolesFound = roleRepository.findAll();
+    //then
+    assertThat(roleRepository.findAll()).isNotEmpty();
+    assertThat(rolesFound.size()).isGreaterThan(1);
   }
 }
