de0a3e88dd3792e9878701e4bf64d6046d078fcf
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-15 13:21:35 -0500

    Feat: Implementation of upload method

--- src/main/java/com/api/scholarships/services/implementation/CloudinaryService.java
+++ src/main/java/com/api/scholarships/services/implementation/CloudinaryService.java
@@ -2,10 +2,12 @@ package com.api.scholarships.services.implementation;
 
 import com.api.scholarships.services.interfaces.CloudService;
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,8 +23,11 @@ public class CloudinaryService implements CloudService {
     }
 
     @Override
-    public Map upload(MultipartFile multipartFile) {
-        return null;
+    public Map upload(MultipartFile multipartFile) throws IOException {
+        File file = convert(multipartFile);
+        Map result= cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        file.delete();
+        return result;
     }
 
     @Override
