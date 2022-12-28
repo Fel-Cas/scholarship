dfba56b6c057518db1f2f465178f4c57396266ce
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-27 23:16:50 -0500

    Feat: Functionality to save a new company

--- src/main/java/com/api/scholarships/controllers/CompanyController.java
+++ src/main/java/com/api/scholarships/controllers/CompanyController.java
@@ -1,10 +1,34 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.constants.Endpoints;
+import com.api.scholarships.dtos.CompanyDTO;
+import com.api.scholarships.dtos.CompanyDTOResponse;
+import com.api.scholarships.entities.Company;
+import com.api.scholarships.mappers.CompanyMapper;
+import com.api.scholarships.services.interfaces.CompanyService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(Endpoints.COMPANIES)
 public class CompanyController {
+
+  @Autowired
+  private CompanyService companyService;
+  @Autowired
+  private CompanyMapper companyMapper;
+
+  @PostMapping
+  public ResponseEntity<CompanyDTOResponse> create(@ModelAttribute @Valid CompanyDTO companyDTO) throws IOException {
+    Company companySaved=companyService.create(companyDTO);
+    return new ResponseEntity<>(companyMapper.companyToCompanyDTOResponse(companySaved), HttpStatus.CREATED);
+  }
 }
--- src/main/java/com/api/scholarships/services/implementation/CompanyServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/CompanyServiceImp.java
@@ -1,18 +1,53 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.CompanyDTO;
 import com.api.scholarships.dtos.CompanyResponse;
 import com.api.scholarships.dtos.CompanyUpdateDTO;
 import com.api.scholarships.entities.Company;
+import com.api.scholarships.entities.Image;
+import com.api.scholarships.entities.User;
+import com.api.scholarships.exceptions.BadRequestException;
+import com.api.scholarships.mappers.CompanyMapper;
+import com.api.scholarships.repositories.CompanyRepository;
+import com.api.scholarships.services.interfaces.CloudService;
 import com.api.scholarships.services.interfaces.CompanyService;
+import com.api.scholarships.services.interfaces.ImageService;
+import com.api.scholarships.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CompanyServiceImp implements CompanyService {
 
+  @Autowired
+  private CompanyRepository companyRepository;
+  @Autowired
+  private CompanyMapper companyMapper;
+  @Autowired
+  private ImageService imageService ;
+  @Autowired
+  private UserService userService;
+
   @Override
-  public Company create(CompanyDTO companyDTO) {
-    return null;
+  public Company create(CompanyDTO companyDTO) throws IOException {
+    validateUniqueInformationCreateCompany(companyDTO);
+    List<User> users = loadUsers(companyDTO.getUsers());
+    Image imageCreated=imageService.save(companyDTO.getImage());
+    return companyRepository.save(
+        Company.builder()
+            .name(companyDTO.getName().toUpperCase())
+            .address(companyDTO.getAddress().toUpperCase())
+            .email(companyDTO.getEmail())
+            .phone(companyDTO.getPhone())
+            .users(users)
+            .image(imageCreated)
+            .build());
   }
 
   @Override
@@ -48,5 +83,21 @@ public class CompanyServiceImp implements CompanyService {
   @Override
   public void changeImage(Long id, Long imageId) {
 
+  }
+
+  protected void validateUniqueInformationCreateCompany(CompanyDTO companyDTO){
+    if(companyRepository.existsByName(companyDTO.getName())){
+      throw new BadRequestException(Messages.MESSAGE_COMPANY_BAD_REQUEST_CREATE_WITH_WRONG_NAME.formatted(companyDTO.getName()));
+    }
+    if (companyRepository.existsByEmail(companyDTO.getEmail())){
+      throw new BadRequestException(Messages.MESSAGE_COMPANY_BAD_REQUEST_CREATE_WITH_WRONG_EMAIL.formatted(companyDTO.getEmail()));
+    }
+    if(companyRepository.existsByPhone(companyDTO.getPhone())){
+      throw new BadRequestException(Messages.MESSAGE_COMPANY_BAD_REQUEST_CREATE_WITH_WRONG_PHONE.formatted(companyDTO.getPhone()));
+    }
+  }
+
+  protected List<User> loadUsers(List<Long> usersId){
+    return usersId.stream().map(userService::getById).toList();
   }
 }
