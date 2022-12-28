c0378e2f823fe3f159576300467e8ad122f181d5
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-27 23:15:48 -0500

    Refactor: change user id type from integer to Long

--- src/main/java/com/api/scholarships/dtos/CompanyDTO.java
+++ src/main/java/com/api/scholarships/dtos/CompanyDTO.java
@@ -4,6 +4,7 @@ package com.api.scholarships.dtos;
 import com.api.scholarships.constants.Messages;
 import jakarta.validation.MessageInterpolator;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,14 +24,14 @@ public class CompanyDTO {
   @Size(min = 8, max = 50, message = Messages.MESSAGE_ADDRESS_SIZE)
   private String address;
   @NotEmpty(message = Messages.MESSAGE_PHONE_REQUIRED)
-  @Size(min = 8, max = 15, message = Messages.MESSAGE_PHONE_SIZE)
+  @Size(min = 7, max = 15, message = Messages.MESSAGE_PHONE_SIZE)
   private String phone;
   @NotEmpty(message = Messages.MESSAGE_EMAIL_REQUIRED)
   @Size(min = 11, max = 50, message = Messages.MESSAGE_EMAIL_SIZE)
   private String email;
-  @NotEmpty(message = Messages.MESSAGE_IMAGE_REQUIRED)
+  @NotNull(message = Messages.MESSAGE_IMAGE_REQUIRED)
   private MultipartFile image;
   @NotEmpty(message = Messages.MESSAGE_USERS_REQUIRED)
   @Size(min = 1, message = Messages.MESSAGE_USERS_SIZE)
-  private List<Integer> users;
+  private List<Long> users;
 }
