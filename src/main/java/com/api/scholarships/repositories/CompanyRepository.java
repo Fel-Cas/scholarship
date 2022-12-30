e96ddd9dac1de5de42fef967b0fa642d0b1cf13a
Andrés Castro <andres.cmonsalve@gmail.com>
2022-12-29 23:17:40 -0500

    Feat: Add method to verify if a user has a company assigned

--- src/main/java/com/api/scholarships/repositories/CompanyRepository.java
+++ src/main/java/com/api/scholarships/repositories/CompanyRepository.java
@@ -1,6 +1,7 @@
 package com.api.scholarships.repositories;
 
 import com.api.scholarships.entities.Company;
+import com.api.scholarships.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,6 @@ public interface CompanyRepository extends JpaRepository<Company,Long> {
   boolean existsByNameAndIdNot(String name, Long id);
   boolean existsByEmailAndIdNot(String email, Long id);
   boolean existsByPhoneAndIdNot(String phone, Long id);
+  boolean existsByUsers(User user);
 
 }
