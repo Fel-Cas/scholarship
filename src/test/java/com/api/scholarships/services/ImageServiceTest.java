b2b5233c1a4883523b07a40262c6bc2ca093f163
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-01 23:42:24 -0500

    Feat: Test ImageService, test to check an exception to save an image when a file that is not an image has been sent

--- src/test/java/com/api/scholarships/services/ImageServiceTest.java
+++ src/test/java/com/api/scholarships/services/ImageServiceTest.java
@@ -1,6 +1,8 @@
 package com.api.scholarships.services;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.entities.Image;
+import com.api.scholarships.exceptions.BadRequestException;
 import com.api.scholarships.repositories.ImageRepository;
 import com.api.scholarships.services.implementation.ImageServiceImp;
 import com.api.scholarships.services.interfaces.CloudService;
@@ -62,5 +64,16 @@ class ImageServiceTest {
     assertEquals(imageSaved.getName(),image.getName());
     assertEquals(imageSaved.getUrl(),image.getUrl());
     assertEquals(imageSaved.getImageId(),image.getImageId());
+  }
+
+  @Test
+  @DisplayName("Test ImageService, test to verify exception when saving an image")
+  void failSave() throws IOException {
+    //given
+    MockMultipartFile imageFile = new MockMultipartFile("imageFile", "test.png", "image/png", new byte[0]);
+    //when
+    BadRequestException exception = assertThrows(BadRequestException.class, () -> imageService.save(imageFile));
+    //then
+    assertEquals(Messages.MESSAGE_IMAGE_NOT_VALID, exception.getMessage());
   }
 }
