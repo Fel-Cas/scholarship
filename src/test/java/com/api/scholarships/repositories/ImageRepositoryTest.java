b0385de2cb83d5c0f4c41883045b85e2429f32b0
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-01 21:42:16 -0500

    Feat: Test to create a new image

--- src/test/java/com/api/scholarships/repositories/ImageRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/ImageRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.api.scholarships.repositories;
 
 import com.api.scholarships.entities.Image;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -14,4 +17,28 @@ class ImageRepositoryTest {
   @Autowired
   private ImageRepository imageRepository;
   private Image image;
+
+  @BeforeEach
+  void init(){
+    image = Image.builder()
+        .id(1L)
+        .name("image1")
+        .imageId("image1")
+        .url("url")
+        .build();
+  }
+
+  @Test
+  void create(){
+    //given
+    //when
+    Image imageSaved = imageRepository.save(image);
+    //then
+    assertNotNull(imageSaved);
+    assertThat(imageSaved.getId()).isGreaterThan(0);
+    assertEquals(imageSaved.getName(),image.getName());
+    assertEquals(imageSaved.getImageId(),image.getImageId());
+    assertEquals(imageSaved.getUrl(),image.getUrl());
+  }
+
 }
