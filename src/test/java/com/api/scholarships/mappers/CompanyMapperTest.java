204098a198b94cf63ce241f685e322b4576457a2
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-07 22:36:04 -0500

    Feat: Creation of the CompanyMapperTest class to test company mapper

---
+++ src/test/java/com/api/scholarships/mappers/CompanyMapperTest.java
@@ -0,0 +1,10 @@
+package com.api.scholarships.mappers;
+
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyMapperTest {
+
+  private CompanyMapper companyMapper = Mappers.getMapper(CompanyMapper.class);
+}
