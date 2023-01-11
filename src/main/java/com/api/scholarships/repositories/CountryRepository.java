13225a0b9e9c32303f97146e4ae2239cc4a7920d
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-10 23:51:25 -0500

    Feat: Creation of the country repository

---
+++ src/main/java/com/api/scholarships/repositories/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.api.scholarships.repositories;
+
+import com.api.scholarships.entities.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
+}
