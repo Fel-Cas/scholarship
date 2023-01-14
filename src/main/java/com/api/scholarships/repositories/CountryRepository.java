7bb54ee3c382ab2366b57f781f921310ddaa5c20
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-13 22:55:14 -0500

    Feat: Creation of the method to search a country by its name

--- src/main/java/com/api/scholarships/repositories/CountryRepository.java
+++ src/main/java/com/api/scholarships/repositories/CountryRepository.java
@@ -4,9 +4,12 @@ import com.api.scholarships.entities.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
   boolean existsByCountryNameAndAbbreviation(String countryName, String abbreviation);
   boolean existsByCountryName(String countryName);
   boolean existsByAbbreviation(String abbreviation);
+  Optional<Country> findByCountryName(String countryName);
 }
