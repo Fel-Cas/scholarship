371192720a6e65a9cba2b9e819cab817d3080da2
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-13 22:22:21 -0500

    Feat: Creation of  methods to validate if a country exists by name , abbreviation and both

--- src/main/java/com/api/scholarships/repositories/CountryRepository.java
+++ src/main/java/com/api/scholarships/repositories/CountryRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
+  boolean existsByCountryNameAndAbbreviation(String countryName, String abbreviation);
+  boolean existsByCountryName(String countryName);
+  boolean existsByAbbreviation(String abbreviation);
 }
