7d83d1ac494751563402880b3b09c3ef0bcfc102
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-13 22:28:14 -0500

    Feat: Functionality to create a new country

--- src/main/java/com/api/scholarships/controllers/CountryController.java
+++ src/main/java/com/api/scholarships/controllers/CountryController.java
@@ -1,10 +1,32 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.constants.Endpoints;
+import com.api.scholarships.dtos.CountryDTO;
+import com.api.scholarships.dtos.CountryDTOResponse;
+import com.api.scholarships.entities.Country;
+import com.api.scholarships.mappers.CountryMapper;
+import com.api.scholarships.services.interfaces.CountryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(Endpoints.COUNTRIES)
 public class CountryController {
+
+  @Autowired
+  private CountryService countryService;
+  @Autowired
+  private CountryMapper countryMapper;
+
+  @PostMapping()
+  public ResponseEntity<CountryDTOResponse> create(@Valid @RequestBody CountryDTO countryDTO){
+    Country countrySaved=countryService.create(countryDTO);
+    return new ResponseEntity(countryMapper.countryToCountryDTOResponse(countrySaved), HttpStatus.CREATED);
+  }
 }
--- src/main/java/com/api/scholarships/services/implementation/CountryServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/CountryServiceImp.java
@@ -1,8 +1,10 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.CountryDTO;
 import com.api.scholarships.dtos.CountryResponse;
 import com.api.scholarships.entities.Country;
+import com.api.scholarships.exceptions.BadRequestException;
 import com.api.scholarships.repositories.CountryRepository;
 import com.api.scholarships.services.interfaces.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,13 @@ public class CountryServiceImp implements CountryService {
 
   @Override
   public Country create(CountryDTO countryDTO) {
-    return null;
+    validateInformationCreateCountry(countryDTO.getCountryName(), countryDTO.getAbbreviation());
+    return countryRepository.save(Country
+        .builder()
+        .countryName(countryDTO.getCountryName().toUpperCase())
+        .abbreviation(countryDTO.getAbbreviation().toUpperCase())
+        .build()
+    );
   }
 
   @Override
@@ -37,5 +45,18 @@ public class CountryServiceImp implements CountryService {
   @Override
   public void delete(Long id) {
 
+  }
+
+  protected void validateInformationCreateCountry(String countryName, String abbreviation){
+
+    if(countryRepository.existsByCountryNameAndAbbreviation(countryName, abbreviation)){
+      throw  new BadRequestException(Messages.MESSAGE_CREATE_COUNTRY_WITH_WRONG_NAME_AND_ABBREVIATION.formatted(countryName, abbreviation));
+    }
+    if(countryRepository.existsByCountryName(countryName)){
+      throw new BadRequestException(Messages.MESSAGE_CREATE_COUNTRY_WITH_WRONG_NAME.formatted(countryName));
+    }
+    if (countryRepository.existsByAbbreviation(abbreviation)){
+      throw new BadRequestException(Messages.MESSAGE_CREATE_COUNTRY_WITH_WRONG_ABBREVIATION.formatted(abbreviation));
+    }
   }
 }
