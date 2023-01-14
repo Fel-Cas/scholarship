1b2c26f19df7f5319043fe700cf7fdec0fef878f
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-13 21:49:16 -0500

    Feat: Creation of the controller country

---
+++ src/main/java/com/api/scholarships/controllers/CountryController.java
@@ -0,0 +1,10 @@
+package com.api.scholarships.controllers;
+
+import com.api.scholarships.constants.Endpoints;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Endpoints.COUNTRIES)
+public class CountryController {
+}
