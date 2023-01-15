688cca4bd95f65a1d49f3cd8c751a5d1659d58df
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-14 21:34:50 -0500

    Feat: Creation of the CountryMapper class to test country mapper

---
+++ src/test/java/com/api/scholarships/mappers/CountryMapperTest.java
@@ -0,0 +1,11 @@
+package com.api.scholarships.mappers;
+
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryMapperTest {
+
+  private CountryMapper countryMapper= Mappers.getMapper(CountryMapper.class);
+
+}
