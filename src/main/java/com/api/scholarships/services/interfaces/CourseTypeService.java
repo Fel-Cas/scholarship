568b582813e50c75eb981ed4dff479ef2d7219e7
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-14 22:47:51 -0500

    Feat:  Creation of the course type service interface with all the methods to be implemented

---
+++ src/main/java/com/api/scholarships/services/interfaces/CourseTypeService.java
@@ -0,0 +1,10 @@
+package com.api.scholarships.services.interfaces;
+
+import com.api.scholarships.dtos.CourseTypeResponse;
+import com.api.scholarships.entities.CourseType;
+
+public interface CourseTypeService {
+  public CourseType findById(Long id);
+  public CourseTypeResponse findAll(int page, int sizePage, String sort, String order);
+
+}
