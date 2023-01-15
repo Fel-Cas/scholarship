6f91f8f0bedbaa8995a85af0b727f84f8f23c161
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-14 20:12:09 -0500

    Feat: Test CountryService, test to create a country

--- src/test/java/com/api/scholarships/services/CountryServiceTest.java
+++ src/test/java/com/api/scholarships/services/CountryServiceTest.java
@@ -1,17 +1,24 @@
 package com.api.scholarships.services;
 
+import com.api.scholarships.dtos.CountryDTO;
 import com.api.scholarships.entities.Country;
 import com.api.scholarships.mappers.CountryMapper;
 import com.api.scholarships.repositories.CountryRepository;
 import com.api.scholarships.services.implementation.CountryServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
 class CountryServiceTest {
@@ -32,5 +39,27 @@ class CountryServiceTest {
         .countryName("BRASIL")
         .abbreviation("BRA")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CountryService, test to create a country")
+  void testCreate(){
+    //given
+    given(countryRepository.existsByCountryNameAndAbbreviation(anyString(), anyString())).willReturn(false);
+    given(countryRepository.existsByCountryName(anyString())).willReturn(false);
+    given(countryRepository.existsByAbbreviation(anyString())).willReturn(false);
+    given(countryRepository.save(any(Country.class))).willReturn(country);
+
+    CountryDTO countryDTO= CountryDTO.builder()
+        .countryName("Brasil")
+        .abbreviation("Bra")
+        .build();
+    //when
+    Country countryCreated=countryService.create(countryDTO);
+    //then
+    assertNotNull(countryCreated);
+    assertEquals(1L, countryCreated.getId());
+    assertEquals(countryDTO.getCountryName().toUpperCase(), countryCreated.getCountryName());
+    assertEquals(countryDTO.getAbbreviation().toUpperCase(), countryCreated.getAbbreviation());
   }
 }
