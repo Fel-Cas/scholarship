f657f743deda52ef6e9382afeddc397b483d268a
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-14 21:38:05 -0500

    Feat: Test CountryMapper, test to pass from country to countryDTOResponse

--- src/test/java/com/api/scholarships/mappers/CountryMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/CountryMapperTest.java
@@ -1,5 +1,9 @@
 package com.api.scholarships.mappers;
 
+import com.api.scholarships.dtos.CountryDTOResponse;
+import com.api.scholarships.entities.Country;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +11,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class CountryMapperTest {
 
   private CountryMapper countryMapper= Mappers.getMapper(CountryMapper.class);
+
+  @Test
+  @DisplayName("Test CountryMapper, test to pass from country to countryDTOResponse")
+  void testToCountryDTOResponse() {
+    //given
+    Country country=Country.builder()
+        .id(1L)
+        .countryName("BRASIL")
+        .abbreviation("BRA")
+        .build();
+    //when
+    CountryDTOResponse countryDTOResponse=countryMapper.countryToCountryDTOResponse(country);
+    //then
+    assertAll(
+        ()->assertEquals(country.getId(),countryDTOResponse.getId()),
+        ()->assertEquals(country.getCountryName(),countryDTOResponse.getCountryName()),
+        ()->assertEquals(country.getAbbreviation(),countryDTOResponse.getAbbreviation()));
+  }
 
 }
