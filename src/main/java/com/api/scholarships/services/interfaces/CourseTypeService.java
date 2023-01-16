2b9a2bfd5934dd97ccf53d18f6864bbdacd4a87d
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 15:03:56 -0500

    Feat: Add in method to find a cpurse type by name in course type service interface

--- src/main/java/com/api/scholarships/services/interfaces/CourseTypeService.java
+++ src/main/java/com/api/scholarships/services/interfaces/CourseTypeService.java
@@ -6,5 +6,6 @@ import com.api.scholarships.entities.CourseType;
 public interface CourseTypeService {
   public CourseType findById(Long id);
   public CourseTypeResponse findAll(int page, int sizePage, String sort, String order);
+  public CourseType findByCourseType(String courseType);
 
 }
