e974fed162e4e268f8a854193b626689a66035f0
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 15:01:54 -0500

    Feat: Creation of the method to find a course type by its name

--- src/main/java/com/api/scholarships/repositories/CourseTypeRepository.java
+++ src/main/java/com/api/scholarships/repositories/CourseTypeRepository.java
@@ -4,6 +4,9 @@ import com.api.scholarships.entities.CourseType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CourseTypeRepository extends JpaRepository<CourseType,Long> {
+  Optional<CourseType> findByCourseType(String courseType);
 }
