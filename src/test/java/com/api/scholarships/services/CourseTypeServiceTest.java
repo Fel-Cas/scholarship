7f29b78a6735e734b574224de4b93f33ffd3e70b
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 16:00:16 -0500

    Feat: Test CourseTypeServce, test to find a course by its id

--- src/test/java/com/api/scholarships/services/CourseTypeServiceTest.java
+++ src/test/java/com/api/scholarships/services/CourseTypeServiceTest.java
@@ -5,13 +5,19 @@ import com.api.scholarships.mappers.CourseTypeMapper;
 import com.api.scholarships.repositories.CourseTypeRepository;
 import com.api.scholarships.services.implementation.CourseTypeServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
@@ -32,5 +38,18 @@ class CourseTypeServiceTest {
         .id(1L)
         .courseType("CURSO CORTO")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CourseTypeServce, test to find a course by its id")
+  void testFindId(){
+    //given
+    given(courseTypeRepository.findById(anyLong())).willReturn(Optional.of(courseType));
+    //when
+    CourseType courseTypeFound=courseTypeService.findById(anyLong());
+    //then
+    assertNotNull(courseTypeFound);
+    assertEquals(courseType.getId(),courseTypeFound.getId());
+    assertEquals(courseType.getCourseType(),courseTypeFound.getCourseType());
   }
 }
