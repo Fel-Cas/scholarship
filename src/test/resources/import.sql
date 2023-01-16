c072f02b8e53aadd27a7be0d31ef5f9014baca5b
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 15:10:50 -0500

    Feat: Creation of the import file to provide the databse with the default data of the entities that only have read operations

---
+++ src/test/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT into course_types (course_type) values ('CURSO CORTO');
+INSERT into course_types (course_type) values ('MAESTRÍA');
+INSERT into course_types (course_type) values ('CARRERA PROFESIONAL');
+INSERT into course_types (course_type) values ('ESPECIALIZACIÓN');
+INSERT into course_types (course_type) values ('BOOTCAMP');
