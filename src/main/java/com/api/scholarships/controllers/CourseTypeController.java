134f55ee9ded0534dbe7f30ff5d53866d8e5d3bc
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 14:26:01 -0500

    Feat: Functionality to find a course type by id

--- src/main/java/com/api/scholarships/controllers/CourseTypeController.java
+++ src/main/java/com/api/scholarships/controllers/CourseTypeController.java
@@ -1,8 +1,14 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.constants.Endpoints;
+import com.api.scholarships.dtos.CourseTypeDTO;
+import com.api.scholarships.entities.CourseType;
+import com.api.scholarships.mappers.CourseTypeMapper;
 import com.api.scholarships.services.interfaces.CourseTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseTypeController {
   @Autowired
   private CourseTypeService courseTypeService;
+  @Autowired
+  private CourseTypeMapper courseTypeMapper;
+
+  @GetMapping(Endpoints.ID)
+  public ResponseEntity<CourseTypeDTO> findById(@PathVariable("id") Long id){
+    CourseType courseFound= courseTypeService.findById(id);
+    return ResponseEntity.ok(courseTypeMapper.typeCourseToTypeCourseDTO(courseFound));
+  }
 }
--- src/main/java/com/api/scholarships/services/implementation/CourseTypeServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/CourseTypeServiceImp.java
@@ -1,11 +1,15 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.CourseTypeResponse;
 import com.api.scholarships.entities.CourseType;
+import com.api.scholarships.exceptions.NotFoundException;
 import com.api.scholarships.repositories.CourseTypeRepository;
 import com.api.scholarships.services.interfaces.CourseTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseTypeServiceImp implements CourseTypeService {
@@ -15,7 +19,11 @@ public class CourseTypeServiceImp implements CourseTypeService {
 
   @Override
   public CourseType findById(Long id) {
-    return null;
+    Optional<CourseType> courseFound=courseTypeRepository.findById(id);
+    if(courseFound.isEmpty()){
+      throw new NotFoundException(Messages.MESSAGE_COURSE_TYPE_NOT_FOUND.formatted(id));
+    }
+    return courseFound.get();
   }
 
   @Override
