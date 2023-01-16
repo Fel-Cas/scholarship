62e1e30ad39129471dc1f6e0a603c7d68ce9dfeb
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 17:45:01 -0500

    Feat: Test CourseTypeMapper, test to pass from a list of CourseType items to the list of CourseTypeDTO items.

--- src/test/java/com/api/scholarships/mappers/CourseTypeMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/CourseTypeMapperTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTypeMapperTest {
@@ -24,7 +26,7 @@ class CourseTypeMapperTest {
 
   @Test
   @DisplayName("Test CourseTypeMapper, test to pass from CourseType class to CourseTypeDTO class")
-  void testCourseTypeMappertoCourseTypeDTO(){
+  void testPassFromCourseTypetoCourseTypeDTO(){
     //given
     //when
     CourseTypeDTO courseTypeDTO=courseTypeMapper.typeCourseToTypeCourseDTO(courseType);
@@ -32,6 +34,21 @@ class CourseTypeMapperTest {
     assertNotNull(courseTypeDTO);
     assertEquals(courseType.getId(),courseTypeDTO.getId());
     assertEquals(courseType.getCourseType(),courseTypeDTO.getCourseType());
+  }
+
+  @Test
+  @DisplayName("Test CourseTypeMapper, test to pass from a list of CourseType items to the list of CourseTypeDTO items.")
+  void testPassFromCourseTypeListToCourseTypeDTOList(){
+    //given
+    List<CourseType> courseTypes=List.of(courseType);
+    //when
+    List<CourseTypeDTO> courseTypeDTOS=courseTypeMapper.typeCoursesToTypeCourseDTOs(courseTypes);
+    //then
+    assertAll(
+        ()->assertEquals(courseTypes.size(),courseTypeDTOS.size()),
+        ()->assertEquals(courseTypes.get(0).getId(),courseTypeDTOS.get(0).getId()),
+        ()->assertEquals(courseTypes.get(0).getCourseType(),courseTypeDTOS.get(0).getCourseType())
+    );
   }
 
 
