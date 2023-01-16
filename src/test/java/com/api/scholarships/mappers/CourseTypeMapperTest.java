3632ddd076091a8ff138c15b9befaea53254e425
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 17:31:47 -0500

    Feat: Creation of the CourseTypeMapperTest class to test course type mapper

---
+++ src/test/java/com/api/scholarships/mappers/CourseTypeMapperTest.java
@@ -0,0 +1,11 @@
+package com.api.scholarships.mappers;
+
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTypeMapperTest {
+
+  private CourseTypeMapper courseTypeMapper=Mappers.getMapper(CourseTypeMapper.class);
+
+}
