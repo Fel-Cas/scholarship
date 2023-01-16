df0af626b29bbdda0932d499104701b5038fffdc
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 17:38:30 -0500

    Feat: Test CourseTypeMapper, test to pass from CourseType class to CourseTypeDTO class

--- src/test/java/com/api/scholarships/mappers/CourseTypeMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/CourseTypeMapperTest.java
@@ -1,5 +1,10 @@
 package com.api.scholarships.mappers;
 
+import com.api.scholarships.dtos.CourseTypeDTO;
+import com.api.scholarships.entities.CourseType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +12,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourseTypeMapperTest {
 
   private CourseTypeMapper courseTypeMapper=Mappers.getMapper(CourseTypeMapper.class);
+  private CourseType courseType;
+
+  @BeforeEach
+  void setUp(){
+    courseType=CourseType.builder()
+        .id(5L)
+        .courseType("BOOTCAMP")
+        .build();
+  }
+
+  @Test
+  @DisplayName("Test CourseTypeMapper, test to pass from CourseType class to CourseTypeDTO class")
+  void testCourseTypeMappertoCourseTypeDTO(){
+    //given
+    //when
+    CourseTypeDTO courseTypeDTO=courseTypeMapper.typeCourseToTypeCourseDTO(courseType);
+    //then
+    assertNotNull(courseTypeDTO);
+    assertEquals(courseType.getId(),courseTypeDTO.getId());
+    assertEquals(courseType.getCourseType(),courseTypeDTO.getCourseType());
+  }
+
 
 }
