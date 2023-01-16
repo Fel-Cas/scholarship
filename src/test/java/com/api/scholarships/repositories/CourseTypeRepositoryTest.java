00ee5374f4dd404724406050c6bd744a4d2a117c
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 15:24:14 -0500

    Feat: Cration of the CourseTypeRepositoryTest class to test course type repository

---
+++ src/test/java/com/api/scholarships/repositories/CourseTypeRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.api.scholarships.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles(profiles = "test")
+class CourseTypeRepositoryTest {
+  @Autowired
+  private CourseTypeRepository courseTypeRepository;
+
+
+}
