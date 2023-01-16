83699af1802b72f633e792624231b3136f6688d8
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-16 16:22:39 -0500

    Feat: Test CourseTypeService, test to find all course types

--- src/test/java/com/api/scholarships/services/CourseTypeServiceTest.java
+++ src/test/java/com/api/scholarships/services/CourseTypeServiceTest.java
@@ -1,11 +1,14 @@
 package com.api.scholarships.services;
 
 import com.api.scholarships.constants.Messages;
+import com.api.scholarships.dtos.CourseTypeDTO;
+import com.api.scholarships.dtos.CourseTypeResponse;
 import com.api.scholarships.entities.CourseType;
 import com.api.scholarships.exceptions.NotFoundException;
 import com.api.scholarships.mappers.CourseTypeMapper;
 import com.api.scholarships.repositories.CourseTypeRepository;
 import com.api.scholarships.services.implementation.CourseTypeServiceImp;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,13 +16,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
@@ -43,7 +52,7 @@ class CourseTypeServiceTest {
   }
 
   @Test
-  @DisplayName("Test CourseTypeServce, test to find a course by its id")
+  @DisplayName("Test CourseTypeService, test to find a course by its id")
   void testFindId(){
     //given
     given(courseTypeRepository.findById(anyLong())).willReturn(Optional.of(courseType));
@@ -56,7 +65,7 @@ class CourseTypeServiceTest {
   }
 
   @Test
-  @DisplayName("Test CourseTypeServce, test to check  an exception when a course type is searched by its id and does not exist")
+  @DisplayName("Test CourseTypeService, test to check  an exception when a course type is searched by its id and does not exist")
   void  testFailFindById(){
     //given
     given(courseTypeRepository.findById(1L)).willReturn(Optional.empty());
@@ -64,6 +73,31 @@ class CourseTypeServiceTest {
     NotFoundException exception=assertThrows(NotFoundException.class, ()->courseTypeService.findById(1L));
     //then
     assertEquals(Messages.MESSAGE_COURSE_TYPE_NOT_FOUND.formatted(1L), exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("Test CourseTypeService, test to find all course types")
+  void testFindAll(){
+    //given
+    CourseTypeDTO courseTypeDTO=CourseTypeDTO.builder()
+        .id(1L)
+        .courseType("CURSO CORTO")
+        .build();
+    Page<CourseType> courseTypes=new PageImpl<>(List.of(courseType));
+    given(courseTypeRepository.findAll(any(Pageable.class))).willReturn(courseTypes);
+    given(courseTypeMapper.typeCoursesToTypeCourseDTOs(List.of(courseType))).willReturn(List.of(courseTypeDTO));
+    //when
+    CourseTypeResponse courseTypeResponse=courseTypeService.findAll(0,10,"id","ASC");
+    //then
+    assertAll(
+        ()->assertNotNull(courseTypeResponse),
+        ()->assertThat(courseTypeResponse.getContent().size()).isGreaterThan(0),
+        ()->assertEquals(0,courseTypeResponse.getNumberPage()),
+        ()->assertEquals(1,courseTypeResponse.getSizePage()),
+        ()->assertEquals(1,courseTypeResponse.getTotalElements()),
+        ()->assertEquals(1,courseTypeResponse.getTotalPages()),
+        ()->assertTrue(courseTypeResponse.isLastOne())
+    );
   }
 
 }
