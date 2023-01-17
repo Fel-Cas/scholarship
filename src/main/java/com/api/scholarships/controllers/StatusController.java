29b537dd9904dce3e534467000c10a4165cba39f
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 11:26:55 -0500

    Feat: Functionality to find all statuses

--- src/main/java/com/api/scholarships/controllers/StatusController.java
+++ src/main/java/com/api/scholarships/controllers/StatusController.java
@@ -1,16 +1,15 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.constants.Endpoints;
+import com.api.scholarships.constants.PaginationRequest;
 import com.api.scholarships.dtos.StatusDTO;
+import com.api.scholarships.dtos.StatusResponse;
 import com.api.scholarships.entities.Status;
 import com.api.scholarships.mappers.StatusMapper;
 import com.api.scholarships.services.interfaces.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(Endpoints.STATUS)
@@ -24,5 +23,15 @@ public class StatusController {
   public ResponseEntity<StatusDTO> getOne(@PathVariable("id") Long id){
     Status statusFound=statusService.findId(id);
     return ResponseEntity.ok(statusMapper.statusToStatusDTO(statusFound));
+  }
+
+  @GetMapping()
+  public ResponseEntity<StatusResponse> getAll(
+      @RequestParam(name = "numberPage",defaultValue = PaginationRequest.DEFAULT_NUMBER_PAGE, required = false) int page,
+      @RequestParam(name = "pageSize",defaultValue = PaginationRequest.DEFAULT_PAGE_SIZE, required = false) int size,
+      @RequestParam(name = "sort",defaultValue = PaginationRequest.DEFAULT_SORT_BY, required = false) String sort,
+      @RequestParam(name = "order",defaultValue = PaginationRequest.DEFAULT_SORT_DIR, required = false) String order
+  ){
+    return ResponseEntity.ok(statusService.findAll(page, size,sort,order));
   }
 }
--- src/main/java/com/api/scholarships/services/implementation/StatusServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/StatusServiceImp.java
@@ -8,6 +8,10 @@ import com.api.scholarships.mappers.StatusMapper;
 import com.api.scholarships.repositories.StatusRepository;
 import com.api.scholarships.services.interfaces.StatusService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -32,7 +36,17 @@ public class StatusServiceImp implements StatusService {
 
   @Override
   public StatusResponse findAll(int page, int size, String sort, String order) {
-    return null;
+    Sort sortDirection=order.equalsIgnoreCase(Sort.Direction.ASC.name())? Sort.by(sort).ascending(): Sort.by(sort).descending();
+    Pageable pageable= PageRequest.of(page,size,sortDirection);
+    Page<Status> statusesFound=statusRepository.findAll(pageable);
+    return StatusResponse.builder()
+        .content(statusMapper.statusToStatusDTO(statusesFound.getContent()))
+        .lastOne(statusesFound.isLast())
+        .numberPage(statusesFound.getNumber())
+        .sizePage(statusesFound.getSize())
+        .totalElements(statusesFound.getTotalElements())
+        .totalPages(statusesFound.getTotalPages())
+        .build();
   }
 
   @Override
