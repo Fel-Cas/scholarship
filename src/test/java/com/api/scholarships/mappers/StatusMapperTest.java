dfcf96b094d373e6e6c94002bdbe4241882f9db3
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 14:28:09 -0500

    Feat: Test StatusMapper, test to pass from a list of Status items to the list of StatusDTO items

--- src/test/java/com/api/scholarships/mappers/StatusMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/StatusMapperTest.java
@@ -2,24 +2,32 @@ package com.api.scholarships.mappers;
 
 import com.api.scholarships.dtos.StatusDTO;
 import com.api.scholarships.entities.Status;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatusMapperTest {
 
   private StatusMapper statusMapper=Mappers.getMapper(StatusMapper.class);
+  private Status status;
+
+  @BeforeEach
+  void setUp(){
+    status=Status.builder()
+        .id(1L)
+        .statusName("VIGENTE")
+        .build();
+  }
 
   @Test
   @DisplayName("Test StatusMapper, test to pass from Status class to StatusDTO class")
   void statusToStatusDTO() {
     //given
-    Status status=Status.builder()
-        .id(1L)
-        .statusName("VIGENTE")
-        .build();
     //when
     StatusDTO statusDTO=statusMapper.statusToStatusDTO(status);
     //then
@@ -27,6 +35,22 @@ class StatusMapperTest {
         ()->assertNotNull(statusDTO),
         ()->assertEquals(status.getId(),statusDTO.getId()),
         ()->assertEquals(status.getStatusName(),statusDTO.getStatusName())
+    );
+  }
+
+  @Test
+  @DisplayName("Test StatusMapper, test to pass from a list of Status items to the list of StatusDTO items")
+  void  listStatusToStatusDTO() {
+    //given
+    List<Status> statuses=List.of(status);
+    //when
+    List<StatusDTO> statusDTOs=statusMapper.statusToStatusDTO(statuses);
+    //then
+    assertAll(
+        ()->assertNotNull(statusDTOs),
+        ()->assertEquals(1,statusDTOs.size()),
+        ()->assertEquals(status.getId(),statusDTOs.get(0).getId()),
+        ()->assertEquals(status.getStatusName(),statusDTOs.get(0).getStatusName())
     );
   }
 
