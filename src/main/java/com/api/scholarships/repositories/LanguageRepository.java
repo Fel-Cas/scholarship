ac33f6039031a7913c11bab61379053a664a5ea0
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 15:21:28 -0500

    Feat: Creation of the language repository

---
+++ src/main/java/com/api/scholarships/repositories/LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.api.scholarships.repositories;
+
+import com.api.scholarships.entities.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+}
