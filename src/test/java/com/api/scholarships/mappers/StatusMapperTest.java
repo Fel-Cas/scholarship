7683b7c650d3af6fe12c7bebbd29d13d3cd3c731
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 14:19:37 -0500

    Feat: Creation of the StatusMapperTest to test status mapper

---
+++ src/test/java/com/api/scholarships/mappers/StatusMapperTest.java
@@ -0,0 +1,11 @@
+package com.api.scholarships.mappers;
+
+import org.mapstruct.factory.Mappers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatusMapperTest {
+
+  private StatusMapper statusMapper=Mappers.getMapper(StatusMapper.class);
+
+}
