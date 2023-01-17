d732f8741dc0e41cf98e4d0beaea8359ff0f278c
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 12:00:25 -0500

    Feat: Test StatusRepository, test to get all statutes

--- src/test/java/com/api/scholarships/repositories/StatusRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/StatusRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -29,5 +31,16 @@ class StatusRepositoryTest {
     assertNotNull(statusSaved);
     assertThat(statusSaved.getId()).isGreaterThan(4);
     assertEquals(statusSaved.getStatusName(),status.getStatusName());
+  }
+
+  @Test
+  @DisplayName("Test StatusRepository, test to get all statutes")
+  void testGetAllStatuses(){
+    //given
+    //when
+    List<Status> statusesFound=statusRepository.findAll();
+    //then
+    assertNotNull(statusesFound);
+    assertEquals(4, statusesFound.size());
   }
 }
