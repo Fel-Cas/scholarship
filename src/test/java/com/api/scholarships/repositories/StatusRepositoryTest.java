690faa01532bf8a95d6c53e3fa0dd7ff0df05c9e
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 11:57:38 -0500

    Feat: Test StatusRepository, test to create a status

--- src/test/java/com/api/scholarships/repositories/StatusRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/StatusRepositoryTest.java
@@ -1,14 +1,33 @@
 package com.api.scholarships.repositories;
 
+import com.api.scholarships.entities.Status;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataJpaTest
 @ActiveProfiles(profiles = "test")
 class StatusRepositoryTest {
   @Autowired
   private StatusRepository statusRepository;
+
+  @Test
+  @DisplayName("Test StatusRepository, test to create a status")
+  void testCreate(){
+    //given
+    Status status=Status.builder()
+        .statusName("PROCESANDO")
+        .build();
+    //when
+    Status statusSaved= statusRepository.save(status);
+    //then
+    assertNotNull(statusSaved);
+    assertThat(statusSaved.getId()).isGreaterThan(4);
+    assertEquals(statusSaved.getStatusName(),status.getStatusName());
+  }
 }
