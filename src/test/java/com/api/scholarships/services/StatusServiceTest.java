d1d88e91a2a096fa758c56839315a3dd4ece3f3c
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 12:23:21 -0500

    Feat: Test StatusService, test to check for an exception when trying to find a status and it doesn't exist

--- src/test/java/com/api/scholarships/services/StatusServiceTest.java
+++ src/test/java/com/api/scholarships/services/StatusServiceTest.java
@@ -1,6 +1,8 @@
 package com.api.scholarships.services;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.entities.Status;
+import com.api.scholarships.exceptions.NotFoundException;
 import com.api.scholarships.mappers.StatusMapper;
 import com.api.scholarships.repositories.StatusRepository;
 import com.api.scholarships.services.implementation.StatusServiceImp;
@@ -51,4 +53,14 @@ class StatusServiceTest {
     assertEquals(statusFound.getStatusName(),status.getStatusName());
   }
 
+  @Test
+  @DisplayName("Test StatusService, test to check for an exception when trying to find a status and it doesn't exist")
+  void fialGetStatusById(){
+    //given
+    given(statusRepository.findById(status.getId())).willReturn(Optional.empty());
+    //when
+    NotFoundException notFoundException=assertThrows(NotFoundException.class,()->statusService.findId(status.getId()));
+    //then
+    assertEquals(Messages.MESSAGE_STATUS_NOT_FOUND.formatted(status.getId()),notFoundException.getMessage());
+  }
 }
