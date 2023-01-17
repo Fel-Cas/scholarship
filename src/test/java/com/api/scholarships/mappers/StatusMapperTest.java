cf9c46c6ce0d295c11b2dbbda665c8b0f8982257
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 14:23:11 -0500

    Feat: Test StatusMapper, test to pass from Status class to StatusDTO class

--- src/test/java/com/api/scholarships/mappers/StatusMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/StatusMapperTest.java
@@ -1,5 +1,9 @@
 package com.api.scholarships.mappers;
 
+import com.api.scholarships.dtos.StatusDTO;
+import com.api.scholarships.entities.Status;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,5 +11,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatusMapperTest {
 
   private StatusMapper statusMapper=Mappers.getMapper(StatusMapper.class);
+
+  @Test
+  @DisplayName("Test StatusMapper, test to pass from Status class to StatusDTO class")
+  void statusToStatusDTO() {
+    //given
+    Status status=Status.builder()
+        .id(1L)
+        .statusName("VIGENTE")
+        .build();
+    //when
+    StatusDTO statusDTO=statusMapper.statusToStatusDTO(status);
+    //then
+    assertAll(
+        ()->assertNotNull(statusDTO),
+        ()->assertEquals(status.getId(),statusDTO.getId()),
+        ()->assertEquals(status.getStatusName(),statusDTO.getStatusName())
+    );
+  }
 
 }
