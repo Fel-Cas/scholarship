5f922df3cb95fcf158fb02902f49dac93067b202
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 10:25:24 -0500

    Feat: Creation of the status repository

---
+++ src/main/java/com/api/scholarships/repositories/StatusRepository.java
@@ -0,0 +1,9 @@
+package com.api.scholarships.repositories;
+
+import com.api.scholarships.entities.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status,Long> {
+}
