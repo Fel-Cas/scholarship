e19ff6fb36733afeb13b74ca774d8047c082610e
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 15:07:41 -0500

    Feat: Test CareerService, test to save a career

--- src/test/java/com/api/scholarships/services/CareerServiceTest.java
+++ src/test/java/com/api/scholarships/services/CareerServiceTest.java
@@ -1,10 +1,13 @@
 package com.api.scholarships.services;
 
+import com.api.scholarships.dtos.CareerDTO;
 import com.api.scholarships.entities.Career;
 import com.api.scholarships.mappers.CareerMapper;
 import com.api.scholarships.repositories.CareerRepository;
 import com.api.scholarships.services.implementation.CareerServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,6 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -30,6 +36,23 @@ class CareerServiceTest {
         .id(1L)
         .careerName("INGENIERIA DE SISTEMAS")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CareerService, test to save a career")
+  void testToSaveCareer() {
+    //given
+    CareerDTO careerDTO=CareerDTO.builder().careerName("INGENIERIA DE SISTEMAS").build();
+    given(careerRepository.existsByCareerName(anyString())).willReturn(false);
+    given(careerRepository.save(any(Career.class))).willReturn(career);
+    //when
+    Career careerSaved=careerService.create(careerDTO);
+    //then
+    assertAll(
+        ()->assertNotNull(careerSaved),
+        ()->assertEquals(career.getId(),careerSaved.getId()),
+        ()->assertEquals(career.getCareerName(),careerSaved.getCareerName())
+    );
   }
 
 }
