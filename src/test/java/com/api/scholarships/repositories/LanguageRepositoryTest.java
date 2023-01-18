c469127d5632d09245229f990e6371a0889c54d4
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 20:06:24 -0500

    Feat: Test LanguageRepository, test to create a language

--- src/test/java/com/api/scholarships/repositories/LanguageRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/LanguageRepositoryTest.java
@@ -1,15 +1,36 @@
 package com.api.scholarships.repositories;
 
+import com.api.scholarships.entities.Language;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
 class LanguageRepositoryTest {
   @Autowired
   private LanguageRepository languageRepository;
+
+  @Test
+  @DisplayName("Test LanguageRepository, test to create a language")
+  void testCreateLanguage(){
+    //given
+    Language language= Language.builder()
+        .languageName("ÁRABE")
+        .build();
+    //when
+    Language languageSaved=languageRepository.save(language);
+    //then
+    assertAll(
+        ()-> assertNotNull(languageSaved),
+        ()-> assertEquals(languageSaved.getLanguageName(),language.getLanguageName()),
+        ()->assertThat(languageSaved.getId()).isGreaterThan(7)
+    );
+  }
 
 }
