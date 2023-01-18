42df41f7ea9d0dfea0e3244958dcbbe0b99630a5
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 12:48:06 -0500

    Feat: Creation of the method to find a career by name

--- src/main/java/com/api/scholarships/repositories/CareerRepository.java
+++ src/main/java/com/api/scholarships/repositories/CareerRepository.java
@@ -4,7 +4,10 @@ import com.api.scholarships.entities.Career;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CareerRepository extends JpaRepository<Career,Long> {
   boolean existsByCareerName(String careerName);
+  Optional<Career> findByCareerName(String careerName);
 }
