3c0635a7de0ddbb11148012ffcda80223f9c5ee2
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 16:19:12 -0500

    Feat: Test CareerMapper, test to pass from Career class to CareerDTOResponse class

--- src/test/java/com/api/scholarships/mappers/CareerMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/CareerMapperTest.java
@@ -1,7 +1,10 @@
 package com.api.scholarships.mappers;
 
+import com.api.scholarships.dtos.CareerDTOResponse;
 import com.api.scholarships.entities.Career;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,6 +19,20 @@ class CareerMapperTest {
         .id(1L)
         .careerName("INGENIERIA DE SISTEMAS")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CareerMapper, test to pass from Career class to CareerDTOResponse class")
+  void testCareerToCareerDTOResponse(){
+    //given
+    //when
+    CareerDTOResponse careerDTOResponse=careerMapper.careerToCareerDTOResponse(career);
+    //then
+    assertAll(
+        ()->assertNotNull(careerDTOResponse),
+        ()->assertEquals(career.getId(),careerDTOResponse.getId()),
+        ()->assertEquals(career.getCareerName(),careerDTOResponse.getCareerName())
+    );
   }
 
 }
