5f62e53f579911c68239a8d947605a91c7bf79d4
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 21:19:26 -0500

    Feat: Test LanguageController, test to find all Languages

--- src/test/java/com/api/scholarships/controllers/LanguageControllerTest.java
+++ src/test/java/com/api/scholarships/controllers/LanguageControllerTest.java
@@ -1,6 +1,7 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.dtos.LanguageDTO;
+import com.api.scholarships.dtos.LanguageResponse;
 import com.api.scholarships.entities.Language;
 import com.api.scholarships.mappers.LanguageMapper;
 import com.api.scholarships.services.interfaces.LanguageService;
@@ -14,14 +15,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(LanguageController.class)
 class LanguageControllerTest {
 
   @MockBean
@@ -58,4 +63,31 @@ class LanguageControllerTest {
         .andExpect(jsonPath("$.languageName").value(language.getLanguageName()));
   }
 
+  @Test
+  @DisplayName("Test LanguageController, test to find all Languages")
+  void testFindAll() throws Exception {
+    //given
+    LanguageResponse languageResponse=LanguageResponse.builder()
+        .content(List.of(languageDTO))
+        .numberPage(0)
+        .sizePage(10)
+        .lastOne(true)
+        .totalPages(1)
+        .totalElements(1L)
+        .build();
+    given(languageService.findAll(anyInt(), anyInt(), anyString(), anyString())).willReturn(languageResponse);
+    //when
+    ResultActions response=client.perform(get(url).contentType(MediaType.APPLICATION_JSON));
+    //then
+    response.andExpect(status().isOk())
+        .andDo(print())
+        .andExpect(jsonPath("$.content").isArray())
+        .andExpect(jsonPath("$.content").isNotEmpty())
+        .andExpect(jsonPath("$.content.size()").value(1))
+        .andExpect(jsonPath("$.numberPage").value(0))
+        .andExpect(jsonPath("$.sizePage").value(10))
+        .andExpect(jsonPath("$.lastOne").value(true))
+        .andExpect(jsonPath("$.totalPages").value(1))
+        .andExpect(jsonPath("$.totalElements").value(1));
+  }
 }
