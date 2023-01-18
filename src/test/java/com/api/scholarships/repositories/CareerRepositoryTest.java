9f37b010d094bc51a997733549054e9314190839
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 14:34:20 -0500

    Feat: Test CareerRepository, test to create a new career

--- src/test/java/com/api/scholarships/repositories/CareerRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/CareerRepositoryTest.java
@@ -2,6 +2,8 @@ package com.api.scholarships.repositories;
 
 import com.api.scholarships.entities.Career;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,5 +23,19 @@ class CareerRepositoryTest {
     career=Career.builder()
         .careerName("ENFERMERIA")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CareerRepository, test to create a new career")
+  void testCreateCareer(){
+    //given
+    //when
+    Career careerSaved=careerRepository.save(career);
+    //when
+    assertAll(
+        ()->assertNotNull(careerSaved),
+        ()->assertEquals(1L, careerSaved.getId()),
+        ()->assertEquals("ENFERMERIA", careerSaved.getCareerName())
+    );
   }
 }
