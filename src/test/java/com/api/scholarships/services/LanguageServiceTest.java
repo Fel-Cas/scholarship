0396e835b8c6834a24cc36a576d2f3df52165ac9
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-17 20:35:18 -0500

    Feat: Test LanguageService, test to find a language by id

--- src/test/java/com/api/scholarships/services/LanguageServiceTest.java
+++ src/test/java/com/api/scholarships/services/LanguageServiceTest.java
@@ -6,13 +6,19 @@ import com.api.scholarships.mappers.LanguageMapper;
 import com.api.scholarships.repositories.LanguageRepository;
 import com.api.scholarships.services.implementation.LanguageServiceImp;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -36,5 +42,20 @@ class LanguageServiceTest {
         .id(4L)
         .languageName("INGLÉS")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test LanguageService, test to find a language by id")
+  void testGetLanguageById(){
+    //given
+    given(languageRepository.findById(anyLong())).willReturn(Optional.of(language));
+    //when
+    Language languageFound = languageService.findById(anyLong());
+    //then
+    assertAll(
+        ()->assertNotNull(languageFound),
+        ()->assertEquals(language.getId(),languageFound.getId()),
+        ()->assertEquals(language.getLanguageName(),languageFound.getLanguageName())
+    );
   }
 }
