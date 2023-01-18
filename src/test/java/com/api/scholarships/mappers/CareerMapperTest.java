21807388cc64b36f68d1c14da502e3f9699906c6
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 16:22:20 -0500

    Feat: Test CareerMapper, test to pass from list of Career items  to list CareerDTOResponse itmes

--- src/test/java/com/api/scholarships/mappers/CareerMapperTest.java
+++ src/test/java/com/api/scholarships/mappers/CareerMapperTest.java
@@ -1,11 +1,14 @@
 package com.api.scholarships.mappers;
 
 import com.api.scholarships.dtos.CareerDTOResponse;
+import com.api.scholarships.dtos.CareerResponse;
 import com.api.scholarships.entities.Career;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,6 +35,22 @@ class CareerMapperTest {
         ()->assertNotNull(careerDTOResponse),
         ()->assertEquals(career.getId(),careerDTOResponse.getId()),
         ()->assertEquals(career.getCareerName(),careerDTOResponse.getCareerName())
+    );
+  }
+
+  @Test
+  @DisplayName("Test CareerMapper, test to pass from list of Career items  to list CareerDTOResponse itmes")
+  void testListOfCareerToCareerDTOResponse(){
+    //given
+    List<Career> careers=List.of(career);
+    //when
+    List<CareerDTOResponse> careerDTOResponseList=careerMapper.careerListToCareerDTOResponseList(careers);
+    //then
+    assertAll(
+        ()->assertNotNull(careerDTOResponseList),
+        ()->assertEquals(careers.size(),careerDTOResponseList.size()),
+        ()->assertEquals(careers.get(0).getId(),careerDTOResponseList.get(0).getId()),
+        ()->assertEquals(careers.get(0).getCareerName(),careerDTOResponseList.get(0).getCareerName())
     );
   }
 
