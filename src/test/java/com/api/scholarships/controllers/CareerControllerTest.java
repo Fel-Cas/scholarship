b34205c4f86317f5be14e8e2d9db279eddec8994
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 16:00:27 -0500

    Feat: Test CareerController, test to create a career

--- src/test/java/com/api/scholarships/controllers/CareerControllerTest.java
+++ src/test/java/com/api/scholarships/controllers/CareerControllerTest.java
@@ -7,14 +7,22 @@ import com.api.scholarships.mappers.CareerMapper;
 import com.api.scholarships.services.interfaces.CareerService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@WebMvcTest(CareerController.class)
 class CareerControllerTest {
   @MockBean
   private CareerService careerService;
@@ -26,6 +34,7 @@ class CareerControllerTest {
   private ObjectMapper objectMapper;
   private Career career;
   private CareerDTOResponse careerDTOResponse;
+  private String url="/api/v1/scholarships/careers";
 
   @BeforeEach
   void setUp(){
@@ -38,5 +47,24 @@ class CareerControllerTest {
         .id(1L)
         .careerName("INGENIRIA DE SISTEMAS")
         .build();
+  }
+
+  @Test
+  @DisplayName("Test CareerController, test to create a career")
+  void testPostCareer() throws Exception {
+    //given
+    CareerDTO careerDTO= new CareerDTO("INGENIERIA DE SISTEMAS");
+    given(careerService.create(careerDTO)).willReturn(career);
+    given(careerMapper.careerToCareerDTOResponse(career)).willReturn(careerDTOResponse);
+    //when
+    ResultActions response=client.perform(post(url).contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(careerDTO)));
+    //then
+    response
+        .andExpect(status().isCreated())
+        .andDo(print())
+        .andExpect(jsonPath("$.id").value(career.getId()))
+        .andExpect(jsonPath("$.careerName").value(career.getCareerName()))
+        .andExpect(content().json(objectMapper.writeValueAsString(careerDTOResponse)));
   }
 }
