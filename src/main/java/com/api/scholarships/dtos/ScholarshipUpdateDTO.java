4f575ca06943d84629fe6a4b24e72256c84317d5
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-18 23:00:27 -0500

    Feat: Creation of the ScholarshipUpdateDTO class that contains information for updating a scholarship

---
+++ src/main/java/com/api/scholarships/dtos/ScholarshipUpdateDTO.java
@@ -0,0 +1,28 @@
+package com.api.scholarships.dtos;
+
+import com.api.scholarships.constants.Messages;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Data
+public class ScholarshipUpdateDTO {
+  @NotEmpty(message = Messages.MESSAGE_DESCRIPTION_REQUIRED)
+  @Size(min=200, message = Messages.MESSAGE_DESCRIPTION_SIZE)
+  private String description;
+  @NotEmpty(message = Messages.MESSAGE_START_DATE_REQUIRED)
+  private LocalDateTime startDate;
+  @NotEmpty(message = Messages.MESSAGE_END_DATE_REQUIRED)
+  private LocalDateTime finishDate;
+  @NotEmpty(message = Messages.MESSAGE_URL_REQUIRED)
+  @URL(message = Messages.MESSAGE_URL_VALID)
+  private String link;
+}
