96603634d1d3df39210e7244779a45099de7cc5a
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-20 18:21:56 -0500

    Feat: Add default values to find all scholarship information by condition or not

--- src/main/java/com/api/scholarships/constants/PaginationRequest.java
+++ src/main/java/com/api/scholarships/constants/PaginationRequest.java
@@ -5,4 +5,6 @@ public class PaginationRequest {
   public static final String DEFAULT_PAGE_SIZE="10";
   public static final String DEFAULT_SORT_BY="id";
   public static final String DEFAULT_SORT_DIR="asc";
+  public static final String DEFAULT_MODEL_CONDITION="DEFAULT";
+  public static final String DEFAULT_ID="1";
 }
