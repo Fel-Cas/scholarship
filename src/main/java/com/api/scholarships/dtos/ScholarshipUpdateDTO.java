e6f2151e1f040d2e7706c2157117521aa66b86c2
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-20 11:12:28 -0500

    Refactor: Add error messages to the field title and change the type of the startDate and finishDate fields.

--- src/main/java/com/api/scholarships/dtos/ScholarshipUpdateDTO.java
+++ src/main/java/com/api/scholarships/dtos/ScholarshipUpdateDTO.java
@@ -2,11 +2,15 @@ package com.api.scholarships.dtos;
 
 import com.api.scholarships.constants.Messages;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.aspectj.bridge.Message;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,16 +19,18 @@ import java.time.LocalDateTime;
 @Builder
 @Data
 public class ScholarshipUpdateDTO {
-  @NotEmpty
-  @Size(min=10)
+  @NotEmpty(message=Messages.MESSAGE_TITLE_IS_REQUIRED)
+  @Size(min=10, message = Messages.MESSAGE_TITLE_SIZE)
   private String title;
   @NotEmpty(message = Messages.MESSAGE_DESCRIPTION_REQUIRED)
   @Size(min=200, message = Messages.MESSAGE_DESCRIPTION_SIZE)
   private String description;
-  @NotEmpty(message = Messages.MESSAGE_START_DATE_REQUIRED)
-  private LocalDateTime startDate;
-  @NotEmpty(message = Messages.MESSAGE_END_DATE_REQUIRED)
-  private LocalDateTime finishDate;
+  @NotNull(message = Messages.MESSAGE_START_DATE_REQUIRED)
+  @DateTimeFormat(pattern =  "yyyy-MM-dd")
+  private Date startDate;
+  @NotNull(message = Messages.MESSAGE_END_DATE_REQUIRED)
+  @DateTimeFormat(pattern =  "yyyy-MM-dd")
+  private Date finishDate;
   @NotEmpty(message = Messages.MESSAGE_URL_REQUIRED)
   @URL(message = Messages.MESSAGE_URL_VALID)
   private String link;
