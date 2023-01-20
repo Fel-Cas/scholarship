7f77688997ad6434edce3ec67990c5ce0c8aaf08
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-19 22:09:12 -0500

    Feat: Fucntionality to create a new scholarship

--- src/main/java/com/api/scholarships/controllers/ScholarshipController.java
+++ src/main/java/com/api/scholarships/controllers/ScholarshipController.java
@@ -1,11 +1,22 @@
 package com.api.scholarships.controllers;
 
 import com.api.scholarships.constants.Endpoints;
+import com.api.scholarships.dtos.ScholarshipDTO;
+import com.api.scholarships.dtos.ScholarshipDTOResponse;
+import com.api.scholarships.entities.Scholarship;
 import com.api.scholarships.mappers.ScholarshipMapper;
 import com.api.scholarships.services.interfaces.ScholarshipService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.text.ParseException;
 
 @RestController
 @RequestMapping(Endpoints.SCHOLARSHIPS)
@@ -14,4 +25,10 @@ public class ScholarshipController {
   private ScholarshipService scholarshipService;
   @Autowired
   private ScholarshipMapper scholarshipMapper;
+
+  @PostMapping
+  public ResponseEntity<ScholarshipDTOResponse> create(@Valid @ModelAttribute ScholarshipDTO scholarshipDTO) throws IOException, ParseException {
+    Scholarship scholarshipSaved=scholarshipService.create(scholarshipDTO);
+    return new ResponseEntity<>(scholarshipMapper.scholarshipToScholarshipDTOResponse(scholarshipSaved), HttpStatus.CREATED);
+  }
 }
--- src/main/java/com/api/scholarships/services/implementation/ScholarshipServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/ScholarshipServiceImp.java
@@ -1,14 +1,26 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
 import com.api.scholarships.dtos.ScholarshipDTO;
 import com.api.scholarships.dtos.ScholarshipResponse;
 import com.api.scholarships.dtos.ScholarshipUpdateDTO;
-import com.api.scholarships.entities.Scholarship;
+import com.api.scholarships.entities.*;
+import com.api.scholarships.exceptions.BadRequestException;
 import com.api.scholarships.mappers.ScholarshipMapper;
 import com.api.scholarships.repositories.ScholarshipRepository;
 import com.api.scholarships.services.interfaces.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ScholarshipServiceImp implements ScholarshipService {
@@ -30,10 +42,31 @@ public class ScholarshipServiceImp implements ScholarshipService {
   private ImageService imageService;
   @Autowired
   private ScholarshipMapper scholarshipMapper;
+  private SimpleDateFormat simpleDateFormat= new SimpleDateFormat("yyyy-MM-dd");
+  private final String DEFAULT_STATUS="VIGENTE";
+
 
   @Override
-  public Scholarship create(ScholarshipDTO scholarshipDTO) {
-    return null;
+  public Scholarship create(ScholarshipDTO scholarshipDTO) throws IOException, ParseException {
+    validateDates(simpleDateFormat.parse(scholarshipDTO.getStartDate()),
+        simpleDateFormat.parse(scholarshipDTO.getFinishDate()));
+    List<Career> careers=loadCareers(scholarshipDTO.getCareers());
+    return scholarshipRepository.save(
+        Scholarship.builder()
+            .title(scholarshipDTO.getTitle().toUpperCase())
+            .description(scholarshipDTO.getDescription())
+            .startDate(simpleDateFormat.parse(scholarshipDTO.getStartDate()))
+            .finishDate(simpleDateFormat.parse(scholarshipDTO.getFinishDate()))
+            .link(scholarshipDTO.getLink())
+            .courseType(courseTypeService.findByCourseType(scholarshipDTO.getCourseType()))
+            .country(countryService.findByName(scholarshipDTO.getCountry()))
+            .status(service.findByName(DEFAULT_STATUS))
+            .language(languageService.findByName(scholarshipDTO.getLanguage()))
+            .image(imageService.save(scholarshipDTO.getImage()))
+            .company(companyService.getOne(scholarshipDTO.getCompany()))
+            .careers(careers)
+            .build()
+    );
   }
 
   @Override
@@ -54,5 +87,17 @@ public class ScholarshipServiceImp implements ScholarshipService {
   @Override
   public ScholarshipResponse findAll() {
     return null;
+  }
+
+  private void validateDates(Date startDate, Date finishDate) {
+    if(!startDate.before(finishDate)){
+      throw new BadRequestException(Messages.MESSAGE_WRONG_DATES);
+    }
+  }
+
+  private List<Career> loadCareers(List<String> careers) {
+    return new HashSet<>(careers)
+        .stream().map(career->careerService.findByName(career))
+        .collect(Collectors.toList());
   }
 }
