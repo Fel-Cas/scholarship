263af22e00d33cc107f3e134ac3eb958dd31df94
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-19 22:08:48 -0500

    Feat: Add support for exceptions to create method

--- src/main/java/com/api/scholarships/services/interfaces/ScholarshipService.java
+++ src/main/java/com/api/scholarships/services/interfaces/ScholarshipService.java
@@ -5,8 +5,11 @@ import com.api.scholarships.dtos.ScholarshipResponse;
 import com.api.scholarships.dtos.ScholarshipUpdateDTO;
 import com.api.scholarships.entities.Scholarship;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 public interface ScholarshipService {
-  public Scholarship create(ScholarshipDTO scholarshipDTO);
+  public Scholarship create(ScholarshipDTO scholarshipDTO) throws IOException, ParseException;
   public Scholarship getById(long id);
   public Scholarship update(ScholarshipUpdateDTO scholarshipUpdateDTO, long id);
   public void delete(long id);
