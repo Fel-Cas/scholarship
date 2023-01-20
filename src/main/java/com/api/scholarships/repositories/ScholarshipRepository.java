ce852a0dea86947fa2038daa73c16fab6e9d35e7
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-20 18:27:28 -0500

    Feat: Add methods to find all the information of scholarships for diferent conditions

--- src/main/java/com/api/scholarships/repositories/ScholarshipRepository.java
+++ src/main/java/com/api/scholarships/repositories/ScholarshipRepository.java
@@ -1,9 +1,17 @@
 package com.api.scholarships.repositories;
 
-import com.api.scholarships.entities.Scholarship;
+import com.api.scholarships.entities.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
+  Page<Scholarship> findByCourseType(CourseType courseType, Pageable pageable);
+  Page<Scholarship> findByCountry(Country country, Pageable pageable);
+  Page<Scholarship> findByStatus(Status status, Pageable pageable);
+  Page<Scholarship> findByLanguage(Language language, Pageable pageable);
+  Page<Scholarship> findByCompany(Company company, Pageable pageable);
+  Page<Scholarship> findByCareers(Career career, Pageable pageable);
 }
