9e33b8c72a0e404f917c8f5c7ef9ecb366497b96
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-20 21:18:39 -0500

    Feat: Creation of the scholarship object with all the attributes for testing the scholarship repository

--- src/test/java/com/api/scholarships/repositories/ScholarshipRepositoryTest.java
+++ src/test/java/com/api/scholarships/repositories/ScholarshipRepositoryTest.java
@@ -1,9 +1,15 @@
 package com.api.scholarships.repositories;
 
-import com.api.scholarships.entities.Scholarship;
+import com.api.scholarships.entities.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,4 +33,65 @@ class ScholarshipRepositoryTest {
   @Autowired
   private CountryRepository countryRepository;
   private Scholarship scholarship;
+
+  @BeforeEach
+  void setUp(){
+    //Save career
+    Career careerSaved= careerRepository.save(Career
+        .builder()
+        .careerName("INGENIRIA DE SISTEMAS")
+        .build()
+    );
+
+    //Save company
+    Company companySaved = Company.builder()
+        .name("Company S.A")
+        .address("Medellin,Antioquia")
+        .phone("123456789")
+        .email("email@emailcom")
+        .id(1L)
+        .createdAt(Instant.now())
+        .updatedAt(Instant.now())
+        .build();
+
+    // Save country
+    Country countrySaved= countryRepository.save( Country
+        .builder()
+        .countryName("Nigeria")
+        .abbreviation("NG")
+        .build()
+    );
+
+    //Find Course type
+    CourseType courseTypeFound=courseTypeRepository.findById(1L).get();
+
+    //Save image
+    Image imageSaved=imageRepository.save(Image.builder()
+        .name("image1")
+        .imageId("image1")
+        .url("url")
+        .build()
+    );
+
+    // Find Language
+    Language languageFound=languageRepository.findById(1L).get();
+
+    // Find status
+    Status statusFound=statusRepository.findById(1L).get();
+
+    scholarship=Scholarship.builder()
+        .title("Mi titulo")
+        .description("Descripción de la beca")
+        .startDate(new Date())
+        .finishDate(new Date())
+        .link("Esto es un link")
+        .courseType(courseTypeFound)
+        .country(countrySaved)
+        .status(statusFound)
+        .language(languageFound)
+        .image(imageSaved)
+        .company(companySaved)
+        .careers(List.of(careerSaved))
+        .build();
+  }
 }
