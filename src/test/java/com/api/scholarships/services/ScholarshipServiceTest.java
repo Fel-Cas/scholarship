c90832e059bdc9539a82f4803243bb1a6a0aeccd
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-23 15:42:10 -0500

    Feat: Test ScholarshipService, test to create a new scholarship

--- src/test/java/com/api/scholarships/services/ScholarshipServiceTest.java
+++ src/test/java/com/api/scholarships/services/ScholarshipServiceTest.java
@@ -1,5 +1,6 @@
 package com.api.scholarships.services;
 
+import com.api.scholarships.dtos.ScholarshipDTO;
 import com.api.scholarships.entities.*;
 import com.api.scholarships.mappers.ScholarshipMapper;
 import com.api.scholarships.repositories.ScholarshipRepository;
@@ -7,11 +8,15 @@ import com.api.scholarships.services.implementation.ScholarshipServiceImp;
 import com.api.scholarships.services.interfaces.*;
 import com.api.scholarships.services.strategyScholarships.ScholarshipContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,6 +25,10 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("Test")
@@ -103,11 +112,12 @@ class ScholarshipServiceTest {
         .build();
 
     scholarship=Scholarship.builder()
+        .id(1L)
         .title("Mi titulo")
         .description("Descripción de la beca")
         .startDate(format.parse("2023-01-01"))
         .finishDate(format.parse("2023-02-02"))
-        .link("Esto es un link")
+        .link("http:localhost:6788/admin/api/scholarships")
         .courseType(courseType)
         .country(country)
         .status(status)
@@ -116,5 +126,48 @@ class ScholarshipServiceTest {
         .company(company)
         .careers(List.of(career))
         .build();
+  }
+
+  @Test
+  @DisplayName("Test ScholarshipService, test to create a new scholarship")
+  void testCreateScholarship() throws Exception{
+    //given
+    ScholarshipDTO scholarshipDTO=ScholarshipDTO.builder()
+        .title("Mi titulo")
+        .description("Descripción de la beca")
+        .startDate("2023-01-01")
+        .finishDate("2023-02-02")
+        .link("http:localhost:6788/admin/api/scholarships")
+        .courseType("BOOTCAMP")
+        .country("COLOMBIA")
+        .language("ESPAÑOL")
+        .image(new MockMultipartFile("imageFile", "test.png", "image/png", "some image".getBytes()))
+        .company(1L)
+        .careers(List.of("INGENIERIA DE SISTEMAS"))
+        .build();
+    given(careerService.findByName(anyString())).willReturn(career);
+    given(courseTypeService.findByCourseType(anyString())).willReturn(courseType);
+    given(countryService.findByName(anyString())).willReturn(country);
+    given(statusService.findByName(anyString())).willReturn(status);
+    given(languageService.findByName(anyString())).willReturn(language);
+    given(imageService.save(any(MultipartFile.class))).willReturn(image);
+    given(scholarshipRepository.save(any(Scholarship.class))).willReturn(scholarship);
+    //when
+    Scholarship scholarshipSaved=scholarshipService.create(scholarshipDTO);
+    //then
+    assertAll(
+        ()->assertNotNull(scholarshipSaved),
+        ()->assertEquals(scholarshipSaved.getDescription(), scholarship.getDescription()),
+        ()->assertEquals(scholarshipSaved.getStartDate(), scholarship.getStartDate()),
+        ()->assertEquals(scholarshipSaved.getFinishDate(), scholarship.getFinishDate()),
+        ()->assertEquals(scholarshipSaved.getLink(), scholarship.getLink()),
+        ()->assertNotNull(scholarshipSaved.getCourseType()),
+        ()->assertNotNull(scholarshipSaved.getCountry()),
+        ()->assertNotNull(scholarshipSaved.getStatus()),
+        ()->assertNotNull(scholarshipSaved.getLanguage()),
+        ()->assertNotNull(scholarshipSaved.getImage()),
+        ()->assertNotNull(scholarshipSaved.getCompany()),
+        ()->assertThat(scholarshipSaved.getCareers().size()).isGreaterThan(0)
+    );
   }
 }
