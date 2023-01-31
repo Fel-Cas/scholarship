53b7e13e09eca5eff2147f338c4899ee179f6a19
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-30 23:00:38 -0500

    Feat: Add strategy to find scholarships by status and country

---
+++ src/main/java/com/api/scholarships/services/strategyScholarships/ScholarshipStatusCountry.java
@@ -0,0 +1,34 @@
+package com.api.scholarships.services.strategyScholarships;
+
+import com.api.scholarships.constants.Variables;
+import com.api.scholarships.entities.Country;
+import com.api.scholarships.entities.Scholarship;
+import com.api.scholarships.entities.Status;
+import com.api.scholarships.repositories.ScholarshipRepository;
+import com.api.scholarships.services.interfaces.CountryService;
+import com.api.scholarships.services.interfaces.StatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public class ScholarshipStatusCountry implements ScholarshipStrategy{
+
+  @Autowired
+  private ScholarshipRepository scholarshipRepository;
+  @Autowired
+  private CountryService countryService;
+  @Autowired
+  private StatusService statusService;
+
+  @Override
+  public ScholarshipType typeStrategy() {
+    return ScholarshipType.STATUS_COUNTRY;
+  }
+
+  @Override
+  public Page<Scholarship> findScholarshipsByCondition(Pageable pageable, Long idCondition) {
+    Status statusFound=statusService.findByName(Variables.STATUS_DEFAULT);
+    Country countryFound=countryService.findById(idCondition);
+    return scholarshipRepository.findByStatusAndCountry(statusFound, countryFound, pageable);
+  }
+}
