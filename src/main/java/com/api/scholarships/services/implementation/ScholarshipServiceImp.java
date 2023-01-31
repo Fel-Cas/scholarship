bd0bf625568d2084acebdf6179d2856871453b9b
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-30 22:12:13 -0500

    Feat: Add transactional annotation

--- src/main/java/com/api/scholarships/services/implementation/ScholarshipServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/ScholarshipServiceImp.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -187,6 +188,7 @@ public class ScholarshipServiceImp implements ScholarshipService {
 
   @Override
   @Scheduled(cron ="0 0 0 * * ?", zone = "America/Bogota")
+  @Transactional
   public void updateScholarshipsStatus() {
     scholarshipRepository.updateScholarshipStatus();
   }
