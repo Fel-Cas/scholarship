827e3f03a707c45ce1fc86fff626df287d1d0b5d
Andrés Castro <andres.cmonsalve@gmail.com>
2023-01-30 22:20:04 -0500

    Feat: Add other options to search scholarships

--- src/main/java/com/api/scholarships/services/strategyScholarships/ScholarshipType.java
+++ src/main/java/com/api/scholarships/services/strategyScholarships/ScholarshipType.java
@@ -3,7 +3,7 @@ package com.api.scholarships.services.strategyScholarships;
 import java.util.Arrays;
 
 public enum ScholarshipType {
-  STATUS,LANGUAGE,CAREER,COMPANY,COUNTRY,COURSETYPE,DEFAULT;
+  STATUS,LANGUAGE,CAREER,COMPANY,COUNTRY,COURSETYPE,DEFAULT,STATUS_LANGUAGE,STATUS_CAREER,STATUS_COMPANY,STATUS_COUNTRY,STATUS_COURSETYPE;
   public static boolean exists(ScholarshipType type ){
     return Arrays.stream(ScholarshipType.values()).anyMatch(type::equals);
   }
