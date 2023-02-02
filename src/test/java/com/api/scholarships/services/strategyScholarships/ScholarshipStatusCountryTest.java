8d6669d39857d41976d5c5885bec66b7502031be
Andrés Castro <andres.cmonsalve@gmail.com>
2023-02-02 11:53:58 -0500

    Feat: Test ScholarshipStatusCountry, test to find scholarship by country and status

--- src/test/java/com/api/scholarships/services/strategyScholarships/ScholarshipStatusCountryTest.java
+++ src/test/java/com/api/scholarships/services/strategyScholarships/ScholarshipStatusCountryTest.java
@@ -1,14 +1,18 @@
 package com.api.scholarships.services.strategyScholarships;
 
+import com.api.scholarships.constants.Variables;
 import com.api.scholarships.entities.*;
 import com.api.scholarships.repositories.ScholarshipRepository;
 import com.api.scholarships.services.interfaces.CountryService;
 import com.api.scholarships.services.interfaces.StatusService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.text.ParseException;
@@ -17,7 +21,10 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -101,5 +108,27 @@ class ScholarshipStatusCountryTest {
         .company(company)
         .careers(careers)
         .build();
+  }
+
+  @Test
+  @DisplayName("Test ScholarshipStatusCountry, test to find scholarship by country and status")
+  void testToFindScholarshipByCountryAndStatus(){
+    //given
+    given(statusService.findByName(Variables.STATUS_DEFAULT)).willReturn(status);
+    given(countryService.findById(anyLong())).willReturn(country);
+    Page<Scholarship> scholarships=new PageImpl<>(List.of(scholarship));
+    Pageable pageable= PageRequest.of(0,1, Sort.by(Sort.Direction.DESC,"id"));
+    given(scholarshipRepository.findByStatusAndCountry(status,country,pageable)).willReturn(scholarships);
+    //when
+    Page<Scholarship> scholarshipsFound=scholarshipStatusCountry.findScholarshipsByCondition(pageable,1L);
+    //then
+    assertAll(
+        ()->assertThat(scholarshipsFound.getContent().size()).isGreaterThan(0),
+        ()->assertThat(scholarshipsFound.getTotalElements()).isEqualTo(1),
+        ()->assertEquals(0,scholarshipsFound.getNumber()),
+        ()->assertEquals(1,scholarshipsFound.getTotalPages()),
+        ()->assertEquals(1,scholarshipsFound.getSize()),
+        ()->assertTrue(scholarshipsFound.isLast())
+    );
   }
 }
