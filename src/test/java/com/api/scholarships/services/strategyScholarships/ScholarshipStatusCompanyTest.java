4424416145d635a0e0166188196406d634b044f2
Andrés Castro <andres.cmonsalve@gmail.com>
2023-02-02 11:40:20 -0500

    Feat: Creation of the ScholarshipStatusCompanyTest to test the strategy to find scholarships by company and status

---
+++ src/test/java/com/api/scholarships/services/strategyScholarships/ScholarshipStatusCompanyTest.java
@@ -0,0 +1,104 @@
+package com.api.scholarships.services.strategyScholarships;
+
+import com.api.scholarships.entities.*;
+import com.api.scholarships.repositories.ScholarshipRepository;
+import com.api.scholarships.services.interfaces.CompanyService;
+import com.api.scholarships.services.interfaces.StatusService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class ScholarshipStatusCompanyTest {
+  @Mock
+  private StatusService statusService;
+  @Mock
+  private CompanyService companyService;
+  @Mock
+  private ScholarshipRepository scholarshipRepository;
+  @InjectMocks
+  private ScholarshipStatusCompany scholarshipStatusCompany;
+  private Status status;
+  private Company company;
+  private Scholarship scholarship;
+  private SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+  @BeforeEach()
+  void setUp() throws ParseException {
+    List<Career> careers=new ArrayList<>();
+
+    //Course type
+    CourseType courseType= CourseType.builder()
+        .id(1L)
+        .courseType("BOOTCAMP")
+        .build();
+    //Country
+    Country country= Country.builder()
+        .id(1L)
+        .countryName("COLOMBIA")
+        .abbreviation("COL")
+        .build();
+    //status
+    Status status= Status.builder()
+        .id(1L)
+        .statusName("ACTIVO")
+        .build();
+    //language
+    Language language=Language.builder()
+        .id(1L)
+        .languageName("ESPAÑOL")
+        .build();
+    //image
+    Image image=Image.builder()
+        .id(1L)
+        .url("https://localhost:8080/images/profile")
+        .imageId("dsdsdsd")
+        .name("profile.png")
+        .build();
+    //career
+    Career career=Career.builder()
+        .id(1L)
+        .careerName("INGENIERIA DE SISTEMAS")
+        .build();
+    careers.add(career);
+    //company
+    company=Company.builder()
+        .name("Company S.A")
+        .address("Medellin,Antioquia")
+        .phone("123456789")
+        .email("email@emailcom")
+        .id(1L)
+        .image(image)
+        .createdAt(Instant.now())
+        .updatedAt(Instant.now())
+        .build();
+
+    scholarship=Scholarship.builder()
+        .id(1L)
+        .title("Mi titulo")
+        .description("Descripción de la beca")
+        .startDate(format.parse("2023-01-01"))
+        .finishDate(format.parse("2023-02-02"))
+        .link("http:localhost:6788/admin/api/scholarships")
+        .courseType(courseType)
+        .country(country)
+        .status(status)
+        .language(language)
+        .image(image)
+        .company(company)
+        .careers(careers)
+        .build();
+  }
+}
