fc2613cd6fc2ed30bcdbbf370c0800acee180795
Andrés Castro <andres.cmonsalve@gmail.com>
2023-02-10 10:42:02 -0500

    Feat: Creation of the implementation to obtain the current user who is making the request

--- src/main/java/com/api/scholarships/services/implementation/CurrentUsersServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/CurrentUsersServiceImp.java
@@ -1,19 +1,12 @@
 package com.api.scholarships.services.implementation;
 
-import com.api.scholarships.entities.User;
-import com.api.scholarships.repositories.UserRepository;
-import com.api.scholarships.services.interfaces.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.api.scholarships.services.interfaces.CurrentUserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CurrentUsersService {
-
-  @Autowired
-  private UserRepository userRepository;
-
+public class CurrentUsersServiceImp implements CurrentUserService {
   public String getCurrentUser() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     return authentication.getName();
