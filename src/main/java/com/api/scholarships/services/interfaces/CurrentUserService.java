b08e625241979ee663e76072725cb63f3b11e98f
Andrés Castro <andres.cmonsalve@gmail.com>
2023-02-10 12:12:12 -0500

    Feat: Creation of the methos to verify property to access company information

--- src/main/java/com/api/scholarships/services/interfaces/CurrentUserService.java
+++ src/main/java/com/api/scholarships/services/interfaces/CurrentUserService.java
@@ -1,7 +1,9 @@
 package com.api.scholarships.services.interfaces;
 
+import com.api.scholarships.entities.Company;
 import com.api.scholarships.entities.User;
 
 public interface CurrentUserService {
   void verifyCorrectUser(User user);
+  void verifyCorrectUserInCompany(Company companyFound);
 }
