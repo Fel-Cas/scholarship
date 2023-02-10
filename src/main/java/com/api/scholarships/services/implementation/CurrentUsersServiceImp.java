6bf819c63abe23332081f62738dc3ce60422af19
Andrés Castro <andres.cmonsalve@gmail.com>
2023-02-10 11:48:08 -0500

    Feat: Implementation of the functionality to verify if the user gonna to access to the information is the user who is makin the request

--- src/main/java/com/api/scholarships/services/implementation/CurrentUsersServiceImp.java
+++ src/main/java/com/api/scholarships/services/implementation/CurrentUsersServiceImp.java
@@ -1,5 +1,8 @@
 package com.api.scholarships.services.implementation;
 
+import com.api.scholarships.constants.Messages;
+import com.api.scholarships.entities.User;
+import com.api.scholarships.exceptions.ForbiddenException;
 import com.api.scholarships.services.interfaces.CurrentUserService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,8 +10,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CurrentUsersServiceImp implements CurrentUserService {
-  public String getCurrentUser() {
+  private String getCurrentUser() {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
     return authentication.getName();
+  }
+
+  @Override
+  public void verifyCorrectUser(User user) {
+    if (!user.getEmail().equals(getCurrentUser())){
+      throw new ForbiddenException(Messages.MESSAGE_ACCESS_DENIED);
+    }
   }
 }
